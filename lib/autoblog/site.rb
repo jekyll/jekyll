bb0fb49aee4b649a414ca5838966a70bc400b4c7
Tom Preston-Werner <tom@mojombo.com>
2008-10-19 20:19:50 -0700

    update read_layouts with better code

--- lib/autoblog/site.rb
+++ lib/autoblog/site.rb
@@ -18,12 +18,12 @@ module AutoBlog
     
     def read_layouts
       base = File.join(self.source, "_layouts")
-      dir = Dir.new(base)
-      dir.each do |f|
-        unless %w{. ..}.include?(f)
-          name = f.split(".")[0..-2].join(".")
-          self.layouts[name] = File.read(File.join(base, f))
-        end
+      entries = Dir.entries(base)
+      entries = entries.reject { |e| File.directory?(e) }
+      
+      entries.each do |f|
+        name = f.split(".")[0..-2].join(".")
+        self.layouts[name] = File.read(File.join(base, f))
       end
     rescue Errno::ENOENT => e
       # ignore missing layout dir
