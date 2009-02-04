6fec047631c0ebcbab8ffba0bd106abe6a8fb12e
Mark <mark@threewordslong.com>
2009-02-04 20:07:35 +1100

    Added ability to set Jekyll parameters via _config.yaml file

--- lib/jekyll.rb
+++ lib/jekyll.rb
@@ -13,22 +13,6 @@ require 'yaml'
 # 3rd party
 require 'liquid'
 require 'redcloth'
-begin
-  require 'maruku'
-  require 'maruku/ext/math'
-  # Switch off MathML output
-  MaRuKu::Globals[:html_math_output_mathml] = false
-  MaRuKu::Globals[:html_math_engine] = 'none'
-
-  # Turn on math to PNG support with blahtex
-  # Resulting PNGs stored in `images/latex`
-  MaRuKu::Globals[:html_math_output_png] = true
-  MaRuKu::Globals[:html_png_engine] =  'blahtex'
-  MaRuKu::Globals[:html_png_dir] = 'images/latex'
-  MaRuKu::Globals[:html_png_url] = '/images/latex/'
-rescue LoadError
-  puts "The maruku gem is required for markdown support!"
-end
 
 # internal requires
 require 'jekyll/core_ext'
@@ -47,10 +31,6 @@ module Jekyll
     attr_accessor :source, :dest, :lsi, :pygments, :markdown_proc,:permalink_style
   end
   
-  Jekyll.lsi = false
-  Jekyll.pygments = false
-  Jekyll.markdown_proc = Proc.new { |x| Maruku.new(x).to_html }
-  Jekyll.permalink_style = :date
   
   def self.process(source, dest)
     require 'classifier' if Jekyll.lsi
