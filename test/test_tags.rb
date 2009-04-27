e0477e32cc091bf1aa163190eaf78a7bb20dabc0
Nick Quaranto <nick@quaran.to>
2009-04-27 08:21:45 -0400

    Adding failing test for RDiscount parsing issue with highlight tags.

--- test/test_tags.rb
+++ test/test_tags.rb
@@ -2,13 +2,18 @@ require File.dirname(__FILE__) + '/helper'
 
 class TestTags < Test::Unit::TestCase
 
-  def create_post(code)
+  def create_post(content, override = {})
     stub(Jekyll).configuration do
-      Jekyll::DEFAULTS.merge({'pygments' => true})
+      Jekyll::DEFAULTS.merge({'pygments' => true}).merge(override)
     end
     site = Site.new(Jekyll.configuration)
     info = { :filters => [Jekyll::Filters], :registers => { :site => site } }
 
+    @result = Liquid::Template.parse(content).render({}, info)
+    @result = site.markdown(@result)
+  end
+
+  def fill_post(code, override = {})
     content = <<CONTENT
 ---
 title: This is a test
@@ -20,14 +25,12 @@ This document results in a markdown error with maruku
 #{code}
 {% endhighlight %}
 CONTENT
-
-    @result = Liquid::Template.parse(content).render({}, info)
-    @result = site.markdown(@result)
+    create_post(content, override)
   end
 
   context "post content has highlight tag" do
     setup do
-      create_post("test")
+      fill_post("test")
     end
 
     should "not cause a markdown error" do
@@ -41,11 +44,51 @@ CONTENT
 
   context "post content has highlight tag with UTF character" do
     setup do
-      create_post("Æ")
+      fill_post("Æ")
     end
 
     should "render markdown with pygments line handling" do
       assert_match %{<pre>Æ\n</pre>}, @result
+    end
+  end
+
+  context "simple post with markdown and pre tags" do
+    setup do
+      @content = <<CONTENT
+---
+title: Maruku vs. RDiscount
+---
+
+_FIGHT!_
+
+{% highlight ruby %}
+puts "3..2..1.."
+{% endhighlight %}
+
+*FINISH HIM*
+CONTENT
+    end
+
+    context "using Maruku" do
+      setup do
+        create_post(@content)
+      end
+
+      should "parse correctly" do
+        assert_match %{<em>FIGHT!</em>}, @result
+        assert_match %{<em>FINISH HIM</em>}, @result
+      end
+    end
+
+    context "using RDiscount" do
+      setup do
+        create_post(@content, 'markdown' => 'rdiscount')
+      end
+
+      should "parse correctly" do
+        assert_match %{<em>FIGHT!</em>}, @result
+        assert_match %{<em>FINISH HIM</em>}, @result
+      end
     end
   end
 end
