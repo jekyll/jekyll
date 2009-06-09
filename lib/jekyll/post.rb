0a1e3cd2508c797d7b8d1038636a6e7111e5cd3d
Thomas Jack <thomasjack@gmail.com>
2009-06-08 19:26:29 -0500

    stop sorting post categories so they show up in order in the url

--- lib/jekyll/post.rb
+++ lib/jekyll/post.rb
@@ -132,7 +132,7 @@ module Jekyll
         "month"      => date.strftime("%m"),
         "day"        => date.strftime("%d"),
         "title"      => CGI.escape(slug),
-        "categories" => categories.sort.join('/')
+        "categories" => categories.join('/')
       }.inject(template) { |result, token|
         result.gsub(/:#{token.first}/, token.last)
       }.gsub(/\/\//, "/")
