6a605753c1ca31983765eb3ce96d3ae01d759b3b
Tom Preston-Werner <tom@mojombo.com>
2010-06-23 15:50:22 -0700

    Need double quotes for newline character. Fixes #178.

--- lib/jekyll/converters/markdown.rb
+++ lib/jekyll/converters/markdown.rb
@@ -3,8 +3,8 @@ module Jekyll
   class MarkdownConverter < Converter
     safe true
 
-    pygments_prefix '\n'
-    pygments_suffix '\n'
+    pygments_prefix "\n"
+    pygments_suffix "\n"
 
     def setup
       return if @setup
