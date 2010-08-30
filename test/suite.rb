e054202c2b0acbd2bf83d85bf473b3a9568985ef
Jason Watson <jbw@jbw.cc>
2010-08-30 22:35:15 +0100

    fix load errors in ruby 1.9.2.

--- test/suite.rb
+++ test/suite.rb
@@ -3,7 +3,7 @@ require 'test/unit'
 # for some reason these tests fail when run via TextMate
 # but succeed when run on the command line.
 
-tests = Dir["#{File.dirname(__FILE__)}/test_*.rb"]
+tests = Dir[File.expand_path("#{File.dirname(__FILE__)}/test_*.rb")]
 tests.each do |file|
   require file
 end
