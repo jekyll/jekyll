de7e0011f9b8d3a59fc6203217990684df7e91f9
Jason Watson <jbw@jbw.cc>
2010-08-31 03:41:00 +0100

    Add some colouring to the unit tests.

--- test/helper.rb
+++ test/helper.rb
@@ -6,9 +6,12 @@ require File.join(File.dirname(__FILE__), *%w[.. lib jekyll])
 require 'RedCloth'
 require 'rdiscount'
 
-require 'test/unit'
 require 'shoulda'
 require 'rr'
+
+require 'test/unit'
+require 'test/unit/ui/console/testrunner'
+class Test::Unit::UI::Console::TestRunner; def guess_color_availability; true; end; end
 
 include Jekyll
 
