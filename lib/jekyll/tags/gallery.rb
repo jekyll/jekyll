fc8940d7fed9b429fac1533fb67d9edd28736086
Robert Bruce Park <rbpark@exolucere.ca>
2011-03-04 01:49:00 -0700

    Allow formats other than just ".jpg", and provide 'title' to the templates.
    'title' is just the image filename with the extension removed, hyphens and underscores converted to spaces, and the remaining words converted to Title Case.

--- lib/jekyll/tags/gallery.rb
+++ lib/jekyll/tags/gallery.rb
@@ -12,29 +12,28 @@
 #   <img src="{{ image }}" />
 #   {% endgallery %}
 #
-# The important point here is that 'your_gallery_name' in the liquid tag
-# matches the directory name that contains the index.html file and the 'img'
-# directory.
+# The important point here is that 'your_gallery_name' matches the directory
+# name that contains both the index.html file and the 'img' directory.
 #
-# If the directory containing your images is called something other than
-# 'img' and you're too lazy to rename it, you can optionally add the dir
-# attribute to the liquid tag to specify the name of the folder. Eg:
+# The gallery plugin takes a few options to customize itself to your setup.
+# 'dir:img' and 'format:jpg' are defaults, so you only need to include those
+# if your images are in a different subfolder than 'img' and your files are
+# named something other than '.jpg'. 'name' is the only mandatory setting.
 #
-#   {% gallery name:your_gallery_name dir:your_photo_dir %}
-#   <img src="{{ image }}" />
-#   {% endgallery %}
+#   {% gallery name:your_gallery_name dir:img format:jpg %}
 #
-# If you want to create a grid layout for your images, you can
-# combine the 'cycle' liquid tag to achieve that like this:
+# If you want to create a grid layout for your images, you can use the 'cycle'
+# tag to achieve that. This example starts a new row after every fourth image:
 #
 #   <table><tr>
 #   {% gallery name:your_gallery_name %}
-#   <td><img src="{{ image }}" /></td>
+#   <td><img title="{{ title }}" src="{{ image }}" /></td>
 #   {% cycle '', '', '', '</tr><tr>' %}
 #   {% endgallery %}
 #   </tr></table>
 #
-# That starts a new row after every fourth image.
+# This example also introduces the 'title' tag. It is simply the filename of
+# the image converted from hyphenated-lowercase to Title Case.
 
 
 module Jekyll
@@ -44,7 +43,6 @@ module Jekyll
     Syntax = /(#{Liquid::QuotedFragment}+)?/
     
     def initialize(tag_name, markup, tokens)
-      @name = 'image'
       @attributes = {}
       
       # Parse parameters
@@ -57,10 +55,15 @@ module Jekyll
       end
       
       if @attributes['name'].nil?
-        raise SyntaxError.new("You did not specify the name of your gallery.")
+        raise ArgumentError.new("You did not specify the name of your gallery.")
       end
       
-      @attributes['dir'] ||= 'img'
+      @attributes['dir']    ||= 'img'
+      @attributes['format'] ||= 'jpg'
+      
+      @name = @attributes['name']
+      @dir  = @attributes['dir']
+      @fmt  = @attributes['format']
       
       super
     end
@@ -68,29 +71,30 @@ module Jekyll
     def render(context)
       context.registers[:gallery] ||= Hash.new(0)
       
-      # Find files ending in '.jpg' and sort them reverse-lexically. This means
-      # that files whose names begin with YYYYMMDD are sorted newest first.
-      images = Dir.glob(File.join(@attributes['name'],
-        @attributes['dir'], '*.jpg')).sort {|x,y| y <=> x }
+      # Find files ending in '.jpg' and sort them reverse-lexically. You can
+      # achieve newest-first sorting by beginning your filenames with YYYYMMDD
+      images = Dir.glob(File.join(@name, @dir, "*.#{@fmt}")).sort {|x,y| y <=> x }
       length = images.length
       result = []
       
       context.stack do
         images.each_with_index do |img, index|
-          # Strip first dir off image name. This turns 'gallery/img/foo.jpg'
-          # into just 'img/foo.jpg', because this plugin is intended to be
-          # used in 'gallery/index.html', giving you nice relative links to
-          # the image files.
-          context[@name] = img.sub(/^[^\/]+\//, '')
+          # Convert hyphens and underscores to spaces, then Title Case the filename,
+          # after stripping off the dirname and file extension.
+          context['title'] = File.basename(img).chomp(".#{@fmt}").split(
+            /[-_]/).map {|word| word.capitalize }.join(' ').sub(/^\d+\s/, '')
+          
+          # Provide relative links instead of absolute ones.
+          context['image'] = img.sub(/^[^\/]+\//, '')
           context['forloop'] = {
-            'name' => @name,
+            'name' => 'gallery',
             'length' => length,
             'index' => index + 1,
             'index0' => index,
             'rindex' => length - index,
             'rindex0' => length - index - 1,
             'first' => (index == 0),
-            'last' => (index == length - 1) 
+            'last' => (index == length - 1)
           }
           
           result << render_all(@nodelist, context)
