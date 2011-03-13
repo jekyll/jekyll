5e87d9cdd77df04a12b2e059a7e609a16d0c2dfd
Robert Bruce Park <rbpark@exolucere.ca>
2011-03-13 00:00:17 -0700

    Code style cleanup.
    Moved the String.titleize method definition into core_ext.rb. Also deleted the copious usage documentation comments because a) it was a bit out of date and b) that belongs on the wiki anyway.

--- lib/jekyll/tags/gallery.rb
+++ lib/jekyll/tags/gallery.rb
@@ -1,52 +1,5 @@
-# Usage: Organize your photo files like this:
-#
-# your_gallery_name/index.html
-# your_gallery_name/img/YYYYMMDD-photo1.jpg
-# your_gallery_name/img/YYYYMMDD-photo2.jpg
-# your_gallery_name/img/YYYYMMDD-photo3.jpg
-# your_gallery_name/img/YYYYMMDD-etc.jpg
-#
-# Your index.html should contain at least this, for the simplest gallery:
-#
-#   {% gallery name:your_gallery_name %}
-#   <img src="{{ file.path }}" />
-#   {% endgallery %}
-#
-# The important point here is that 'your_gallery_name' matches the directory
-# name that contains both the index.html file and the 'img' directory.
-#
-# The gallery plugin takes a few options to customize itself to your setup.
-# 'dir:img' and 'format:jpg' are defaults, so you only need to include those
-# if your images are in a different subfolder than 'img' and your files are
-# named something other than '.jpg'. 'name' is the only mandatory setting.
-#
-#   {% gallery name:your_gallery_name dir:img format:jpg reverse:no %}
-#
-# By default, the gallery will be sorted reverse-lexically. If you've named
-# your files with the YYYYMMDD prefix, this results in a newest-first sorting.
-# If you desire the sort to be the other way around, specify 'reverse:no'.
-#
-# If you want to create a grid layout for your images, you can use the 'cycle'
-# tag to achieve that. This example starts a new row after every fourth image:
-#
-#   <table><tr>
-#   {% gallery name:your_gallery_name %}
-#   <td><img title="{{ file.title }}" src="{{ file.path }}" /></td>
-#   {% cycle '', '', '', '</tr><tr>' %}
-#   {% endgallery %}
-#   </tr></table>
-#
-# This example also introduces the 'file.title' tag. It is simply the filename of
-# the image converted from hyphenated-lowercase to Title Case.
-
-class String
-  def titleize
-    split(/[\W_]+/).map(&:capitalize).join ' '
-  end
-end
-
-
 module Jekyll
+
   class GalleryTag < Liquid::Block
     include Convertible
     attr_accessor :content, :data
@@ -123,7 +76,9 @@ module Jekyll
       end
       result
     end
+  
   end
+
 end
 
 Liquid::Template.register_tag('gallery', Jekyll::GalleryTag)
