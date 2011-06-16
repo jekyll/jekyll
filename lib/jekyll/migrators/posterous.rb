e1b5f116cb107dadd40664540653b04e2286b71d
Pepijn de Vos <pepijndevos@gmail.com>
2011-06-16 05:43:30 -0700

    Update Posterous migrator using the official posterous gem and processing images

--- lib/jekyll/migrators/posterous.rb
+++ lib/jekyll/migrators/posterous.rb
@@ -1,68 +1,88 @@
 require 'rubygems'
 require 'jekyll'
 require 'fileutils'
+require 'posterous'
 require 'net/http'
-require 'uri'
-require "json"
+require 'URI'
 
-# ruby -r './lib/jekyll/migrators/posterous.rb' -e 'Jekyll::Posterous.process(email, pass, blog)'
+Posterous.config = {
+  'username'  => ARGV[0],
+  'password'  => ARGV[1],
+  'api_token' => ARGV[2]
+}
 
-module Jekyll
-  module Posterous
-    def self.fetch(uri_str, limit = 10)
-      # You should choose better exception.
-      raise ArgumentError, 'Stuck in a redirect loop. Please double check your email and password' if limit == 0
+include Posterous
 
-      response = nil
-      Net::HTTP.start('posterous.com') do |http|
-        req = Net::HTTP::Get.new(uri_str)
-        req.basic_auth @email, @pass
-        response = http.request(req)
-      end
+puts "all set"
 
-      case response
-        when Net::HTTPSuccess     then response
-        when Net::HTTPRedirection then fetch(response['location'], limit - 1)
-        else response.error!
-      end
-    end
+FileUtils.mkdir_p "_posts"
+FileUtils.mkdir_p "images"
 
-    def self.process(email, pass, blog = 'primary')
-      @email, @pass = email, pass
-      @api_token = JSON.parse(self.fetch("/api/2/auth/token").body)['api_token']
-      FileUtils.mkdir_p "_posts"
+site = Site.primary
+page = 1
+posts = site.posts(:page => page)
 
-      posts = JSON.parse(self.fetch("/api/v2/users/me/sites/#{blog}/posts?api_token=#{@api_token}").body)
-      page = 1
+puts "made it so far"
 
-      while posts.any?
-        posts.each do |post|
-          title = post["title"]
-          slug = title.gsub(/[^[:alnum:]]+/, '-').downcase
-          date = Date.parse(post["display_date"])
-          content = post["body_html"]
-          published = !post["is_private"]
-          name = "%02d-%02d-%02d-%s.html" % [date.year, date.month, date.day, slug]
+def download_image(u)
+	path = 'images/%s' % u.split('/')[-1]
+	url = URI.parse(u)
+	found = false 
+	until found 
+		host, port = url.host, url.port if url.host && url.port 
+		query = url.query ? url.query : ""
+		req = Net::HTTP::Get.new(url.path + '?' + query)
+		res = Net::HTTP.start(host, port) {|http|  http.request(req) } 
+		res.header['location'] ? url = URI.parse(res.header['location']) : found = true 
+	end 
+	open(path, "wb") do |file|
+		file.write(res.body)
+	end
+	path
+end	
 
-          # Get the relevant fields as a hash, delete empty fields and convert
-          # to YAML for the header
-          data = {
-             'layout' => 'post',
-             'title' => title.to_s,
-             'published' => published
-           }.delete_if { |k,v| v.nil? || v == ''}.to_yaml
+while posts.any?
+	posts.each do |post|
+		puts post.title
+		slug = post.slug.gsub('/', '-')
+		date = Date.parse(post.display_date)
+		published = !post.is_private
+		name = "%02d-%02d-%02d-%s.html" % [date.year, date.month, date.day, slug]
 
-          # Write out the data and content to file
-          File.open("_posts/#{name}", "w") do |f|
-            f.puts data
-            f.puts "---"
-            f.puts content
-          end
-        end
+		tags = post.tags.map do |t|
+			t['name']
+		end
+		# Get the relevant fields as a hash, delete empty fields and convert
+		# to YAML for the header
+		data = {
+			'layout' => 'post',
+			'title' => post.title.to_s,
+			'published' => published,
+			'tags' => tags,
+		}.delete_if { |k,v| v.nil? || v == ''}.to_yaml
 
-        page += 1
-        posts = JSON.parse(self.fetch("/api/v2/users/me/sites/#{blog}/posts?api_token=#{@api_token}&page=#{page}").body)
-      end
-    end
-  end
+		content = post.body_html
+
+		# awefull hack, do not use on vlog or podcast
+		post.media[2]['images'].each do |img|
+			path = download_image(img['full']['url'])
+			tag = "<img src=\"/%s\" alt=\"%s\" />" % [path, img['full']['caption']]
+			puts tag
+			begin
+				content[/\[\[posterous-content:[^\]]*\]\]/] = tag
+			rescue IndexError
+				puts "weird stuff happening"
+			end
+		end
+
+		# Write out the data and content to file
+		File.open("_posts/#{name}", "w") do |f|
+			f.puts data
+			f.puts "---"
+			f.puts content
+		end
+	end
+
+	page += 1
+	posts = site.posts(:page => page)
 end
