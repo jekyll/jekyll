03ad973523b6ac313014dd5f0678a6da05f0183e
Tom Preston-Werner <tom@mojombo.com>
2012-01-21 19:43:03 -0800

    Update history.

--- lib/jekyll/post.rb
+++ lib/jekyll/post.rb
@@ -131,7 +131,7 @@ module Jekyll
           "title"      => CGI.escape(slug),
           "i_day"      => date.strftime("%d").to_i.to_s,
           "i_month"    => date.strftime("%m").to_i.to_s,
-          "categories" => categories.map {|c| URI.escape(c) }.join('/'),
+          "categories" => categories.map { |c| URI.escape(c) }.join('/'),
           "output_ext" => self.output_ext
         }.inject(template) { |result, token|
           result.gsub(/:#{Regexp.escape token.first}/, token.last)
