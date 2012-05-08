1eff42ae4fa7c0c0f6ca2389a01265d38f8dde42
Paul and Kyle <kburton@relaynetwork.com>
2012-05-08 15:02:49 -0400

    look for js file relative to current page, support a list from the front-matter js_includes

--- lib/jekyll/tags/javascript.rb
+++ lib/jekyll/tags/javascript.rb
@@ -6,8 +6,8 @@ module Jekyll
       @file = file
     end
 
-    def find_javascript(context)
-      file = @file
+    def find_javascript(original_file,context)
+      file = original_file
       file.strip!
       file.gsub! /^["']/, ""
       file.gsub! /["']$/, ""
@@ -15,30 +15,45 @@ module Jekyll
         file = "#{file}.js"
       end
 
-      files = [File.join(context.registers[:site].source, @file),
-               # strip a leading slash
-               File.join(context.registers[:site].source, @file[1..-1]),
+      source_dir = context.registers[:site].source
+      page_url = context.environments.first['page']['url']
+      files = [File.join(source_dir, file),
+               # try with stripping a leading slash
+               File.join(source_dir, file[1..-1]),
+               # try relative to the current page
+               File.join(source_dir, File.dirname(page_url), file)
                ]
       files.each {|file|
         if File.exists? file
           return file
         end
       }
-      return file
+      warn "Javascript file: '#{original_file}' not found"
+      return original_file
     end
 
-    def render(context)
-      file = find_javascript(context)
-
-      mtime = nil
-      if ! File.exists?(file)
-        warn "Javascript file: '#{@file}' not found (#{file})"
-        mtime = Time.now.to_i
-      else
-        mtime = File.mtime(file).to_i
+    def render(context, *stuff)
+      files = context.environments.first['page']['js_includes']
+      if !@file.nil? && !@file.empty?
+        files = [@file]
       end
 
-      return %Q{<script src="#{@file}?#{mtime}" type="text/javascript" ></script>}
+      result = []
+      files.each do |file|
+        next if file.nil? || file.empty?
+        mfile = find_javascript(file,context)
+
+        mtime = nil
+        if ! File.exists?(mfile)
+          mtime = Time.now.to_i
+        else
+          mtime = File.mtime(mfile).to_i
+        end
+
+        result << %Q{<script src="#{file}?#{mtime}" type="text/javascript" ></script>}
+      end
+
+      result.join("")
     end
   end
 
