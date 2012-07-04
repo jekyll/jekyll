d9a461521fba51ab2834b905797c5602cd71850b
Claas Abert <claas@cabird.de>
2012-07-04 13:09:32 +0200

    Fixes include tag patch for #176 , @file variable is no longer modified

--- lib/jekyll/tags/include.rb
+++ lib/jekyll/tags/include.rb
@@ -7,27 +7,27 @@ module Jekyll
     end
 
     def render(context)
-      @file = context[@file] if context.has_key?(@file)
+      file = context[@file] || @file
       includes_dir = File.join(context.registers[:site].source, '_includes')
 
       if File.symlink?(includes_dir)
         return "Includes directory '#{includes_dir}' cannot be a symlink"
       end
 
-      if @file !~ /^[a-zA-Z0-9_\/\.-]+$/ || @file =~ /\.\// || @file =~ /\/\./
-        return "Include file '#{@file}' contains invalid characters or sequences"
+      if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
+        return "Include file '#{file}' contains invalid characters or sequences"
       end
 
       Dir.chdir(includes_dir) do
         choices = Dir['**/*'].reject { |x| File.symlink?(x) }
-        if choices.include?(@file)
-          source = File.read(@file)
+        if choices.include?(file)
+          source = File.read(file)
           partial = Liquid::Template.parse(source)
           context.stack do
             partial.render(context)
           end
         else
-          "Included file '#{@file}' not found in _includes directory"
+          "Included file '#{file}' not found in _includes directory"
         end
       end
     end
