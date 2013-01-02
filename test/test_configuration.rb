d0e2a0aab7287ba639650c9714515b8ac7489040
Parker Moore <parkrmoore@gmail.com>
2013-01-01 21:07:18 -0500

    Tests for --config

--- test/test_configuration.rb
+++ test/test_configuration.rb
@@ -26,4 +26,31 @@ class TestConfiguration < Test::Unit::TestCase
       assert_equal Jekyll::DEFAULTS, Jekyll.configuration({})
     end
   end
+  context "loading config from external file" do
+    setup do
+      @paths = {
+        :default => File.join(Dir.pwd, '_config.yml'),
+        :other   => File.join(Dir.pwd, '_config.live.yml'),
+        :empty   => ""
+      }
+    end
+
+    should "load default config if no config_file is set" do
+      mock(YAML).load_file(@paths[:default]) { Hash.new }
+      mock($stdout).puts("Configuration from #{@paths[:default]}")
+      assert_equal Jekyll::DEFAULTS, Jekyll.configuration({})
+    end
+
+    should "load different config if specified" do
+      mock(YAML).load_file(@paths[:other]) { {"baseurl" => "http://wahoo.dev"} }
+      mock($stdout).puts("Configuration from #{@paths[:other]}")
+      assert_equal Jekyll::DEFAULTS.deep_merge({ "baseurl" => "http://wahoo.dev" }), Jekyll.configuration({ "config" => @paths[:other] })
+    end
+
+    should "load default config if path passed is empty" do
+      mock(YAML).load_file(@paths[:default]) { Hash.new }
+      mock($stdout).puts("Configuration from #{@paths[:default]}")
+      assert_equal Jekyll::DEFAULTS, Jekyll.configuration({ "config" => @paths[:empty] })
+    end
+  end
 end
