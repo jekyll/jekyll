ba48870eadd48cdd2e2981a5904e164e915b2320
Eric Theise <erictheise@gmail.com>
2013-01-10 12:29:04 -0800

    outputting full path when file does not parse

--- test/test_convertible.rb
+++ test/test_convertible.rb
@@ -20,20 +20,26 @@ class TestConvertible < Test::Unit::TestCase
     end
 
     should "not parse if there is syntax error in front-matter" do
+      @name = 'broken_front_matter2.erb'
       out = capture_stdout do
-        ret = @convertible.read_yaml(@base, 'broken_front_matter2.erb')
+        ret = @convertible.read_yaml(@base, @name)
         assert_equal({}, ret)
       end
       assert_match(/YAML Exception|syntax error/, out)
+      assert_match(/#{@base}/, out)
+      assert_match(/#{@name}/, out)
     end
 
     if RUBY_VERSION >= '1.9.2'
       should "not parse if there is encoding error in file" do
+        @name = 'broken_front_matter3.erb'
         out = capture_stdout do
-          ret = @convertible.read_yaml(@base, 'broken_front_matter3.erb')
+          ret = @convertible.read_yaml(@base, @name)
           assert_equal({}, ret)
         end
         assert_match(/invalid byte sequence in UTF-8/, out)
+        assert_match(/#{@base}/, out)
+        assert_match(/#{@name}/, out)
       end
     end
   end
