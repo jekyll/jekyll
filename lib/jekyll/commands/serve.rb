e03f48085a0937bb41d7717243f497da82bd1972
Tom Bell <tomb@tomb.io>
2013-01-19 22:36:46 +0000

    Update command classes moving into a module

--- lib/jekyll/commands/serve.rb
+++ lib/jekyll/commands/serve.rb
@@ -1,28 +1,28 @@
 module Jekyll
+  module Commands
+    class Serve < Command
+      def self.process(options)
+        require 'webrick'
+        include WEBrick
 
-  class ServeCommand < Command
-    def self.process(options)
-      require 'webrick'
-      include WEBrick
+        destination = options['destination']
 
-      destination = options['destination']
+        FileUtils.mkdir_p(destination)
 
-      FileUtils.mkdir_p(destination)
+        mime_types = WEBrick::HTTPUtils::DefaultMimeTypes
+        mime_types.store 'js', 'application/javascript'
 
-      mime_types = WEBrick::HTTPUtils::DefaultMimeTypes
-      mime_types.store 'js', 'application/javascript'
+        s = HTTPServer.new(
+          :Port => options['port'],
+          :BindAddress => options['host'],
+          :MimeTypes => mime_types
+        )
 
-      s = HTTPServer.new(
-        :Port => options['port'],
-        :BindAddress => options['host'],
-        :MimeTypes => mime_types
-      )
-
-      s.mount(options['baseurl'], HTTPServlet::FileHandler, destination)
-      t = Thread.new { s.start }
-      trap("INT") { s.shutdown }
-      t.join()
+        s.mount(options['baseurl'], HTTPServlet::FileHandler, destination)
+        t = Thread.new { s.start }
+        trap("INT") { s.shutdown }
+        t.join()
+      end
     end
   end
-
 end
