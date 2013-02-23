425f46684203b8c3d7a5c51d1ddd76b594c95cd0
sterebooster <stereobooster@gmail.com>
2013-02-24 00:38:10 +0100

    'gist' liquid tag by @stereobooster. Fixes #463.

---
+++ lib/jekyll/tags/gist.rb
@@ -0,0 +1,19 @@
+# Gist Liquid Tag
+#
+# Example:
+#    {% gist 1234567 %}
+
+module Jekyll
+  class GistTag < Liquid::Tag
+    def initialize(tag_name, gist, tokens)
+      super
+      @gist = gist
+    end
+
+    def render(context)
+      "<script src=\"http://gist.github.com/#{@gist}.js\" type=\"text/javascript\"></script>"
+    end
+  end
+end
+
+Liquid::Template.register_tag('gist', Jekyll::GistTag)
