815c281ab51438cb9d81396f5bab6d5b2d7432c8
Daniel Grieve <daniel.grieve@neo.com>
2013-02-27 17:34:30 +0000

    Document remaining internal methods

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -24,17 +24,23 @@ module Jekyll
         end
       end
 
+      # Internal: Processes the sample post template
+      #
+      # Returns the processed ERB post template file content as a String
       def self.scaffold_post_content(template_site)
         ERB.new(File.read(File.expand_path(sample_post_path, template_site))).result
       end
 
       # Internal: Gets the filename of the sample post to be created
       #
-      # Returns the filename of the sample post, as a String
+      # Returns the filename of the sample post as a String
       def self.initialized_post_name
         "_posts/#{Time.now.strftime('%Y-%m-%d')}-welcome-to-jekyll.markdown"
       end
 
+      # Internal: Gets the path of the file containing the sample post template
+      #
+      # Returns the filename of the sample post template as a String
       def self.sample_post_path
         "_posts/0000-00-00-sample_post.markdown.erb"
       end
