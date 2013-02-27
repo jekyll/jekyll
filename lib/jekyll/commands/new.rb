3815a3dbca3e295fa0368c37c1a81621e3377545
Daniel Grieve <daniel.grieve@neo.com>
2013-02-27 09:18:40 +0000

    Fix copying of site template files

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -11,20 +11,21 @@ module Jekyll
           puts "You must provide a path."
         else
           path = File.expand_path(args_joined, Dir.pwd)
-          FileUtils.mkdir_p path
+
           template_site = File.expand_path("../../site_template", File.dirname(__FILE__))
-          FileUtils.mkdir_p path
-          sample_files = Dir["#{template_site}/**/*"].reject {|f| File.extname(f) == ".erb"}
-          FileUtils.cp_r sample_files, path
-          File.open(File.expand_path(self.initialized_post_name, path), "w") do |f|
-            f.write(self.scaffold_post_content(template_site))
+          FileUtils.cp_r template_site, path
+
+          File.open(File.expand_path(initialized_post_name, path), "w") do |f|
+            f.write(scaffold_post_content(template_site))
           end
+          File.unlink(File.expand_path(sample_post_path, path))
+
           puts "New jekyll site installed in #{path}."
         end
       end
 
       def self.scaffold_post_content(template_site)
-        ERB.new(File.read(File.expand_path("_posts/0000-00-00-sample_post.markdown.erb", template_site))).result
+        ERB.new(File.read(File.expand_path(sample_post_path, template_site))).result
       end
 
       # Internal: Gets the filename of the sample post to be created
@@ -32,6 +33,10 @@ module Jekyll
       # Returns the filename of the sample post, as a String
       def self.initialized_post_name
         "_posts/#{Time.now.strftime('%Y-%m-%d')}-welcome-to-jekyll.markdown"
+      end
+
+      def self.sample_post_path
+        "_posts/0000-00-00-sample_post.markdown.erb"
       end
     end
   end
--- test/test_new_command.rb
+++ test/test_new_command.rb
@@ -2,33 +2,56 @@ require 'helper'
 require 'jekyll/commands/new'
 
 class TestNewCommand < Test::Unit::TestCase
-  context 'when args contains a site name' do
+  def dir_contents(path)
+    Dir["#{path}/**/*"].each do |file|
+      file.gsub! path, ''
+    end
+  end
+
+  context 'when args contains a path' do
     setup do
-      @site_name = 'new-site'
-      @args = [@site_name]
-      @site_path = File.expand_path(@site_name, Dir.pwd)
+      @path = 'new-site'
+      @args = [@path]
+      @full_path = File.expand_path(@path, Dir.pwd)
     end
 
     teardown do
-      FileUtils.rm_r @site_path
+      FileUtils.rm_r @full_path
     end
 
     should 'create a new directory' do
-      assert !Dir.exists?(@site_name)
+      assert !Dir.exists?(@full_path)
       capture_stdout { Jekyll::Commands::New.process(@args) }
-      assert Dir.exists?(@site_name)
+      assert Dir.exists?(@full_path)
     end
 
     should 'display a success message' do
       output = capture_stdout { Jekyll::Commands::New.process(@args) }
-      success_message = "New jekyll site installed in #{@site_path}.\n"
+      success_message = "New jekyll site installed in #{@full_path}.\n"
       assert_equal success_message, output
+    end
+
+    should 'copy the site template to the new directory' do
+      stubbed_date = "2013-01-01"
+      stub.instance_of(Time).strftime { stubbed_date }
+
+      site_template = File.expand_path("../lib/site_template", File.dirname(__FILE__))
+      site_template_files = dir_contents(site_template).reject do |f|
+        File.extname(f) == ".erb"
+      end
+      site_template_files << "/_posts/#{stubbed_date}-welcome-to-jekyll.markdown"
+
+      capture_stdout { Jekyll::Commands::New.process(@args) }
+
+      new_site_files = dir_contents(@full_path)
+
+      assert_same_elements site_template_files, new_site_files
     end
   end
 
   context 'when multiple args are given' do
     setup do
-      @site_name_with_spaces = 'new site name/with/paths'
+      @site_name_with_spaces = 'new site name'
       @multiple_args = @site_name_with_spaces.split
     end
 
