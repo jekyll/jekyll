781140954ff86f4221368540c3e3ce4e07c7b8a4
maul.esel <maul.esel@go4more.de>
2013-04-07 12:49:19 +0200

    fix param parsing as suggested by @mojombo
    This changes the syntax: instead of comma-separated,
    params are now separated by spaces and the values are
    enclosed in quotes.

--- lib/jekyll/tags/include.rb
+++ lib/jekyll/tags/include.rb
@@ -3,12 +3,58 @@ module Jekyll
     class IncludeTag < Liquid::Tag
       def initialize(tag_name, params, tokens)
         super
-        @file = params
 
-        separator = params.index(' ')
-        @file = params[0..separator].strip if params.include?(' ')
+	if (params.include?(' '))
+	  separator = params.index(' ')
+          @file = params[0..separator].strip if params.include?(' ')
+          params(params[separator..-1])
+	else
+          @file = params
+	end
+      end
 
-        @params = params[separator..-1].split(',').map{|h| h1,h2 = h.split('='); {h1.strip => h2.strip} if (h1 && h2) }.reduce(:merge) if defined?(separator)
+      def params(markup)
+	last_space = last_quote = last_pos = pos = 0
+	last_key = nil
+	in_quotes = false
+	@params = {}
+
+        while pos = markup.index(/[=\"\s]/, pos)
+	  str = markup[pos]
+	  if /\s/ =~ str
+	    last_space = pos
+	  elsif str == '='
+	    if last_key != nil
+	      raise SyntaxError.new <<-eos
+Syntax Error in tag 'include' (missing value) while parsing the following markup:
+
+  #{markup}
+
+Valid syntax: include param="value"
+eos
+            end
+	    last_key = markup[last_space+1..pos-1]
+	  elsif (str == '"' || str == "'") && markup[pos - 1] != '\\'
+	    in_quotes = !in_quotes
+	    if !in_quotes
+	      value = markup[last_quote+1..pos-1].gsub(/\\"/, '"')
+	      @params[last_key] = value
+	      last_key = nil
+	    end
+	    last_quote = pos
+          end
+	  last_pos = pos += 1
+        end
+
+	if in_quotes
+	  raise SyntaxError.new <<-eos
+Syntax Error in tag 'include' (unterminated value) while parsing the following markup:
+
+  #{markup}
+
+Valid syntax: include param="value"
+eos
+        end
       end
 
       def render(context)
