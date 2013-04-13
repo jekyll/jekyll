61465053eb367dadf117230d4ae0e7f236570c45
Parker Moore <parkrmoore@gmail.com>
2013-04-13 18:02:16 +0200

    Add Jekyll::Logger

---
+++ lib/jekyll/logger.rb
@@ -0,0 +1,42 @@
+module Jekyll
+  module Logger
+    # Public: Print a jekyll message to stdout
+    #
+    # topic - the topic of the message, e.g. "Configuration file", "Deprecation", etc.
+    # message - the message detail
+    #
+    # Returns nothing
+    def info(topic, message)
+      $stdout.puts Jekyll.message(topic, message)
+    end
+
+    # Public: Print a jekyll message to stderr
+    #
+    # topic - the topic of the message, e.g. "Configuration file", "Deprecation", etc.
+    # message - the message detail
+    #
+    # Returns nothing
+    def warn(topic, message)
+      $stderr.puts Jekyll.message(topic, message)
+    end
+
+    # Public: Build a Jekyll topic method
+    #
+    # topic - the topic of the message, e.g. "Configuration file", "Deprecation", etc.
+    # message - the message detail
+    #
+    # Returns the formatted message
+    def message(topic, message)
+       formatted_topic(topic) + message.gsub(/\s+/, ' ')
+    end
+
+    # Public: Format the topic
+    #
+    # topic - the topic of the message, e.g. "Configuration file", "Deprecation", etc.
+    #
+    # Returns the formatted topic statement
+    def formatted_topic(topic)
+      "#{topic} ".rjust(20)
+    end
+  end
+end
