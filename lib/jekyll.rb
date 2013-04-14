7a7990fb9afce1606d9d4e487f5ae5148860132b
Parker Moore <parkrmoore@gmail.com>
2013-04-14 05:23:42 +0200

    Moved markdown converters into lib/jekyll/converters/markdown

--- lib/jekyll.rb
+++ lib/jekyll.rb
@@ -46,7 +46,7 @@ require 'jekyll/command'
 
 require_all 'jekyll/commands'
 require_all 'jekyll/converters'
-require_all 'jekyll/converters/parsers'
+require_all 'jekyll/converters/markdown'
 require_all 'jekyll/generators'
 require_all 'jekyll/tags'
 
