3feb18bb56b0b94e9caf9f5647a662f775b058ab
Parker Moore <parkrmoore@gmail.com>
2013-05-04 15:29:43 +0200

    Redcarpet Parser requires pygments

--- lib/jekyll/converters/markdown/redcarpet_parser.rb
+++ lib/jekyll/converters/markdown/redcarpet_parser.rb
@@ -4,13 +4,12 @@ module Jekyll
       class RedcarpetParser
         def initialize(config)
           require 'redcarpet'
+          require 'pygments'
           @config = config
           @redcarpet_extensions = {}
           @config['redcarpet']['extensions'].each { |e| @redcarpet_extensions[e.to_sym] = true }
 
           @renderer ||= Class.new(Redcarpet::Render::HTML) do
-            require 'pygments'
-
             def block_code(code, lang)
               lang = lang && lang.split.first || "text"
               output = add_code_tags(
