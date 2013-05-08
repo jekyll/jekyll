f67f7f3db43e64c9fc174d3a8e6e6651cffe93f8
Matt Rogers <mattrogers@sbcglobal.net>
2013-05-07 22:27:38 -0500

    Move the building of related posts into their own class

--- lib/jekyll/post.rb
+++ lib/jekyll/post.rb
@@ -1,3 +1,5 @@
+require 'jekyll/related_posts'
+
 module Jekyll
   class Post
     include Comparable
@@ -244,29 +246,7 @@ module Jekyll
     #
     # Returns an Array of related Posts.
     def related_posts(posts)
-      return [] unless posts.size > 1
-
-      if self.site.lsi
-        build_index
-
-        related = self.class.lsi.find_related(self.content, 11)
-        related - [self]
-      else
-        (posts - [self])[0..9]
-      end
-    end
-
-    def build_index
-      self.class.lsi ||= begin
-        puts "Starting the classifier..."
-        lsi = Classifier::LSI.new(:auto_rebuild => false)
-        $stdout.print("  Populating LSI... "); $stdout.flush
-        self.site.posts.each { |x| $stdout.print("."); $stdout.flush; lsi.add_item(x) }
-        $stdout.print("\n  Rebuilding LSI index... ")
-        lsi.build_index
-        puts ""
-        lsi
-      end
+      Jekyll::RelatedPosts.new(self).build
     end
 
     # Add any necessary layouts to this post.
