556c9c1e9cc5e5cb0e5b2816e7afa2234594c85f
Parker Moore <parkrmoore@gmail.com>
2013-06-05 11:40:00 +0200

    Use Symbol log-levels and convert to Integer where needed.

--- lib/jekyll/stevenson.rb
+++ lib/jekyll/stevenson.rb
@@ -1,19 +1,33 @@
 module Jekyll
   class Stevenson
-    attr_accessor :log_level
+    attr_reader :log_level
 
-    DEBUG  = 0
-    INFO   = 1
-    WARN   = 2
+    DEBUG = 0
+    INFO  = 1
+    WARN  = 2
     ERROR = 3
 
     # Public: Create a new instance of Stevenson, Jekyll's logger
     #
-    # level - (optional, integer) the log level
+    # level - a Symbol indicating the log level. One of:
+    #   - :debug
+    #   - :info
+    #   - :warn
+    #   - :error
     #
     # Returns nothing
-    def initialize(level = INFO)
-      @log_level = level
+    def initialize(level = :info)
+      log_level = level
+    end
+
+    # Public: Set the log level
+    #
+    # level - a Symbol indicating the log level.
+    #
+    # Returns nothing.
+    def log_level=(level)
+      @log_level = level.to_sym
+      @log_level_int = log_level_as_int(@log_level)
     end
 
     # Public: Print a jekyll message to stdout
@@ -23,7 +37,7 @@ module Jekyll
     #
     # Returns nothing
     def info(topic, message)
-      $stdout.puts(message(topic, message)) if log_level <= INFO
+      $stdout.puts(message(topic, message)) if @log_level_int <= INFO
     end
 
     # Public: Print a jekyll message to stderr
@@ -33,7 +47,7 @@ module Jekyll
     #
     # Returns nothing
     def warn(topic, message)
-      $stderr.puts(message(topic, message).yellow) if log_level <= WARN
+      $stderr.puts(message(topic, message).yellow) if @log_level_int <= WARN
     end
 
     # Public: Print a jekyll error message to stderr
@@ -43,7 +57,7 @@ module Jekyll
     #
     # Returns nothing
     def error(topic, message)
-      $stderr.puts(message(topic, message).red) if log_level <= ERROR
+      $stderr.puts(message(topic, message).red) if @log_level_int <= ERROR
     end
 
     # Public: Build a Jekyll topic method
@@ -63,6 +77,30 @@ module Jekyll
     # Returns the formatted topic statement
     def formatted_topic(topic)
       "#{topic} ".rjust(20)
+    end
+
+    private
+
+    # Private: Determine the integer representation of the given log level.
+    #
+    # level - a Symbol representing the log level
+    #
+    # Throws: ArgumentError when the log level is invalid.
+    #
+    # Returns an integer representation of the log level.
+    def log_level_as_int(level)
+      case level
+      when :debug
+        DEBUG
+      when :info
+        INFO
+      when :warn
+        WARN
+      when :error
+        ERROR
+      else
+        raise ArgumentError.new("The log level '#{level}' is not a valid log level.")
+      end
     end
   end
 end
