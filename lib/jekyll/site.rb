32e55e77176e9efe55b76e61234fd10fc2463e85
Pat Hawks <pat@pathawks.com>
2013-08-16 21:01:09 -0700

    Use proper Ruby syntax for constant

--- lib/jekyll/site.rb
+++ lib/jekyll/site.rb
@@ -312,7 +312,7 @@ module Jekyll
     def site_payload
       {"site" => self.config.merge({
           "time"       => self.time,
-          "version"    => Jekyll.VERSION,
+          "version"    => Jekyll::VERSION,
           "posts"      => self.posts.sort { |a, b| b <=> a },
           "pages"      => self.pages,
           "html_pages" => self.pages.reject { |page| !page.html? },
