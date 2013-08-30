30a2c501faccf9e2c0a73ecd01bf7884995458eb
maul.esel <maul.esel@go4more.de>
2013-08-30 21:57:51 +0200

    slightly refactor IncludeFragment

--- lib/jekyll/include_fragment.rb
+++ lib/jekyll/include_fragment.rb
@@ -2,16 +2,23 @@ module Jekyll
   class IncludeFragment
     include Convertible
 
-    attr_reader :site, :placeholder, :name
-    attr_accessor :content, :data, :output
+    attr_accessor :content #, :data
+    attr_reader :site, :name
 
-    def initialize(site, container, file, placeholder)
+    def initialize(site, container, file)
       @site = site
       @container = container
       @name = file
-      @placeholder = placeholder
 
+      self.read
+    end
+
+    def read
       self.content = File.read(self.path)
+    end
+
+    def placeholder
+      @placeholder ||= "<# special include placeholder #{Random.rand}#>"
     end
 
     def ext
@@ -21,5 +28,10 @@ module Jekyll
     def path
       File.join(@site.dest, '_includes', @name)
     end
+
+    # Just undef them here temporarily. Take this out before merge.
+    undef_method :write
+    undef_method :do_layout
+    undef_method :render_all_layouts
   end
 end
