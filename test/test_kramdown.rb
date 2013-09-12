ed58ba15082333fe9f0f287288ce33a62202232e
maul.esel <maul.esel@go4more.de>
2013-09-12 22:34:05 +0200

    add tests for moving of coderay settings for kramdown

--- test/test_kramdown.rb
+++ test/test_kramdown.rb
@@ -12,9 +12,17 @@ class TestKramdown < Test::Unit::TestCase
           'footnote_nr'   => 1,
           'entity_output' => 'as_char',
           'toc_levels'    => '1..6',
-          'smart_quotes'  => 'lsquo,rsquo,ldquo,rdquo'
+          'smart_quotes'  => 'lsquo,rsquo,ldquo,rdquo',
+
+          'use_coderay'   => true,
+          'coderay_bold_every'=> 12,
+          'coderay' => {
+            'coderay_css'        => :style,
+            'coderay_bold_every' => 8
+          }
         }
       }
+      @config = Jekyll.configuration(@config)
       @markdown = Converters::Markdown.new(@config)
     end
 
@@ -29,6 +37,26 @@ class TestKramdown < Test::Unit::TestCase
       override = { 'kramdown' => { 'smart_quotes' => 'lsaquo,rsaquo,laquo,raquo' } }
       markdown = Converters::Markdown.new(@config.deep_merge(override))
       assert_match /<p>(&#171;|«)Pit(&#8250;|›)hy(&#187;|»)<\/p>/, markdown.convert(%{"Pit'hy"}).strip
+    end
+
+    context "moving up nested coderay options" do
+      setup do
+        @markdown.convert('some markup')
+        @converter_config = @markdown.instance_variable_get(:@config)['kramdown']
+      end
+
+      should "work correctly" do
+        assert_equal :style, @converter_config['coderay_css']
+      end
+
+      should "also work for defaults" do
+        default = Jekyll::Configuration::DEFAULTS['kramdown']['coderay']['coderay_tab_width']
+        assert_equal default, @converter_config['coderay_tab_width']
+      end
+
+      should "not overwrite" do
+        assert_equal 12, @converter_config['coderay_bold_every']
+      end
     end
   end
 end
