a90ab2e158493490a3768ef8a20bca91017bd8e6
Matthew Iversen <teh.ivo@gmail.com>
2013-10-29 22:45:39 +1100

    Strip any surrounding newlines from produced XHTML

--- lib/jekyll/converters/markdown/maruku_parser.rb
+++ lib/jekyll/converters/markdown/maruku_parser.rb
@@ -41,7 +41,7 @@ module Jekyll
         end
 
         def convert(content)
-          converted = Maruku.new(content, :error_stream => @errors).to_html
+          converted = Maruku.new(content, :error_stream => @errors).to_html.strip
           print_errors_and_fail unless @errors.empty?
           converted
         end
