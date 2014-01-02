c7b649f792d81c386ec85c57a2c0f0ff873d9e43
Parker Moore <parkrmoore@gmail.com>
2014-01-01 19:53:42 -0800

    Update test_generated_site.rb to know about new post

--- test/test_generated_site.rb
+++ test/test_generated_site.rb
@@ -14,7 +14,7 @@ class TestGeneratedSite < Test::Unit::TestCase
     end
 
     should "ensure post count is as expected" do
-      assert_equal 37, @site.posts.size
+      assert_equal 38, @site.posts.size
     end
 
     should "insert site.posts into the index" do
