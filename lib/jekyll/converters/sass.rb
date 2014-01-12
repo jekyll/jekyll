22d4e2aa9005305c727b9bc110af928d8eb78a6d
Parker Moore <parkrmoore@gmail.com>
2014-01-12 14:58:17 -0800

    Consolidate into one master 'Sass' converter.

--- lib/jekyll/converters/sass.rb
+++ lib/jekyll/converters/sass.rb
@@ -4,7 +4,7 @@ module Jekyll
     priority :low
 
     def matches(ext)
-      ext =~ /^\.sass$/i
+      ext =~ /^\.s(a|c)ss$/i
     end
 
     def output_ext(ext)
@@ -12,11 +12,37 @@ module Jekyll
     end
 
     def sass_build_configuration_options(overrides)
-      (@config["sass"] || {}).deep_merge(overrides)
+      (@config["sass"] || {}).deep_merge(overrides).symbolize_keys
+    end
+
+    def syntax_type_of_content(content)
+      if content.include?(";") || content.include?("{")
+        :scss
+      else
+        :sass
+      end
+    end
+
+    def sass_dir
+      @config["source"]["sass"]["sass_dir"] || "_sass"
+    end
+
+    def sass_dir_relative_to_site_source
+      File.join(
+        @config["source"],
+        File.expand_path("/", sass_dir)
+      )
     end
 
     def convert(content)
-      ::Sass.compile(content, sass_build_configuration_options({"syntax" => :sass}))
+      syntax  = syntax_type_of_content(content)
+      configs = sass_build_configuration_options({
+        "syntax" => syntax_type_of_content(content),
+        "cache"  => false,
+        "filesystem_importer" => NilClass,
+        "load_paths" => sass_dir_relative_to_site_source
+      })
+      ::Sass.compile(content, configs)
     end
   end
 end
--- test/test_sass.rb
+++ test/test_sass.rb
@@ -17,4 +17,22 @@ CSS
       assert_equal @output, Jekyll::Sass.new.convert(@content)
     end
   end
+
+  context "converting SCSS" do
+    setup do
+      @content = <<-SCSS
+$font-stack: Helvetica, sans-serif;
+body {
+  font-family: $font-stack;
+  font-color: fuschia;
+}
+SCSS
+      @output = <<-CSS
+body {\n  font-family: Helvetica, sans-serif;\n  font-color: fuschia; }
+CSS
+    end
+    should "produce CSS" do
+      assert_equal @output, Jekyll::Sass.new.convert(@content)
+    end
+  end
 end
