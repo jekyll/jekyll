c36a6d3e0dfd989615b1023fc88c369be8f88bf4
Matt Rogers & Persa Zula <mattrogers@sbcglobal.net>
2014-02-06 23:07:38 -0600

    Wrap the LayoutReader in the Jekyll module

--- lib/jekyll/layout_reader.rb
+++ lib/jekyll/layout_reader.rb
@@ -1,40 +1,40 @@
-class LayoutReader
-  attr_reader :site
-  def initialize(site)
-    @site = site
-    @layouts = {}
-  end
-
-  def read
-    layout_entries.each do |f|
-      @layouts[layout_name(f)] = Layout.new(site, layout_directory, f)
+module Jekyll
+  class LayoutReader
+    attr_reader :site
+    def initialize(site)
+      @site = site
+      @layouts = {}
     end
 
-    @layouts
-  end
+    def read
+      layout_entries.each do |f|
+        @layouts[layout_name(f)] = Layout.new(site, layout_directory, f)
+      end
 
-  private
-
-  def layout_entries
-    entries = []
-    within(layout_directory) do
-      entries = EntryFilter.new(site).filter(Dir['**/*.*'])
+      @layouts
     end
-    entries
+
+    private
+
+    def layout_entries
+      entries = []
+      within(layout_directory) do
+        entries = EntryFilter.new(site).filter(Dir['**/*.*'])
+      end
+      entries
+    end
+
+    def layout_name(file)
+      file.split(".")[0..-2].join(".")
+    end
+
+    def within(directory)
+      return unless File.exists?(directory)
+      Dir.chdir(directory) { yield }
+    end
+
+    def layout_directory
+      File.join(site.source, site.config['layouts'])
+    end
   end
-
-  def layout_name(file)
-    file.split(".")[0..-2].join(".")
-  end
-
-  def within(directory)
-    return unless File.exists?(directory)
-    Dir.chdir(directory) { yield }
-  end
-
-  def layout_directory
-    File.join(site.source, site.config['layouts'])
-  end
-
-
 end
