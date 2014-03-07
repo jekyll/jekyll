b9ee5f4dbdf5faeea542a83c3daba28ee1121366
Parker Moore <parkrmoore@gmail.com>
2014-03-07 15:36:01 -0500

    Fix up Jekyll::Collection

--- lib/jekyll/collection.rb
+++ lib/jekyll/collection.rb
@@ -1,10 +1,12 @@
-module Jekylll
+module Jekyll
   class Collection
     attr_reader :name, :site
+    attr_accessor :relative_directory
 
     def initialize(site, collection_name)
       @site = site
       @name = collection_name
+      @relative_directory = "_#{collection_name}"
     end
 
     def documents
@@ -12,7 +14,7 @@ module Jekylll
     end
 
     def directory
-      "_#{name}"
+      Jekyll.sanitized_path(site.source, relative_directory)
     end
 
     def read_documents
@@ -29,13 +31,38 @@ module Jekylll
       entries
     end
 
-    def within(dir)
+    # Public: Execute a block inside a given directory
+    #
+    # dir   - the directory to change to
+    # block - the block to call
+    #
+    # Returns the return value of the block being called
+    def within(dir, &block)
       return unless File.exists?(dir)
-      Dir.chdir(dir) { yield }
+      Dir.chdir(dir) { block.call }
     end
 
+    # Public: Fetch the class for the collection documents
+    #
+    # Returns the class that should be instantiated for each item in the
     def klass_for_collection
-      const_get(name.split("_").map(&:capitalize).join(""))
+      class_name = name.split("_").map{ |n| n.capitalize.sub(/s\z/, "") }.join("")
+      if Jekyll.const_defined?(:"#{class_name}")
+        Jekyll.const_get(class_name)
+      else
+        Jekyll::Document
+      end
+    end
+
+    # Public: Writes each of the documents to the destination directory
+    #
+    # Returns a list of the filepaths written to the destination
+    def write_output
+      within(site.destination) do
+        documents.each do |document|
+          File.open(document.destination, 'wb') { File.write(document.output) }
+        end
+      end
     end
 
   end
