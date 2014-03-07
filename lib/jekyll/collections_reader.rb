fad53f825ce60f30b9d8cff38cdd7c48c9c72514
Parker Moore <parkrmoore@gmail.com>
2014-03-07 15:36:08 -0500

    Fix CollectionsReader

--- lib/jekyll/collections_reader.rb
+++ lib/jekyll/collections_reader.rb
@@ -2,27 +2,31 @@ module Jekyll
   class CollectionsReader
     attr_reader :site
 
+    # Public: Create a new Collections Reader
+    #
+    # Returns nothing
     def initialize(site)
       @site = site
       @collections = {}
     end
 
+    # Public: Read in all the collections
+    #
+    # Returns the collections which have been read in
     def read
       collection_names.each do |collection_name|
         @collections[collection_name] = Collection.new(site, collection_name)
+        @collections[collection_name].documents
       end
 
       @collections
     end
 
-    private
-
+    # Public: Fetch the collection names to read in
+    #
+    # Returns an array of collection names
     def collection_names
-      site.config["collections"].merge(["posts"])
-    end
-
-    def collection_layout(name)
-      File.join(site.source, "_#{name}")
+      [site.config["collections"], %w[posts]].flatten
     end
   end
 end
