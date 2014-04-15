29fdc0946a2a7e312ddc07f9e22e4bd4db308e8b
Parker Moore <parkrmoore@gmail.com>
2014-04-15 09:46:53 -0400

    Use +rb switches when reading in file for checking existence of YAML header.
    Fixes #2228.

--- lib/jekyll/site.rb
+++ lib/jekyll/site.rb
@@ -419,7 +419,7 @@ module Jekyll
     end
 
     def has_yaml_header?(file)
-      !!(File.open(file).read =~ /\A---\r?\n/)
+      !!(File.open(file, "rb").read =~ /\A---\r?\n/)
     end
 
     def limit_posts!
