e91a90899c2fcc7afdfd0b5cac4900d2e4842b7d
Ben Balter <ben.balter@github.com>
2014-04-18 18:12:47 -0400

    do the do

--- test/test_filters.rb
+++ test/test_filters.rb
@@ -93,7 +93,7 @@ class TestFilters < Test::Unit::TestCase
       assert_equal "&lt;code&gt;command &amp;lt;filename&amp;gt;&lt;/code&gt;", @filter.xml_escape("<code>command &lt;filename&gt;</code>")
     end
 
-    should "not error when xml escaping nil"
+    should "not error when xml escaping nil" do
       assert_equal "", @filter.xml_escape(nil)
     ned
 
