4e4e27b4e102c1d5812b131e8c08302652cc5ff5
Peter Rhoades <createdbypete@gmail.com>
2014-06-12 20:21:26 +0100

    Never use unless with else, switch with positive case first

--- lib/jekyll.rb
+++ lib/jekyll.rb
@@ -136,10 +136,10 @@ module Jekyll
   def self.sanitized_path(base_directory, questionable_path)
     clean_path = File.expand_path(questionable_path, fs_root)
     clean_path.gsub!(/\A\w\:\//, "/")
-    unless clean_path.start_with?(base_directory)
-      File.join(base_directory, clean_path)
-    else
+    if clean_path.start_with?(base_directory)
       clean_path
+    else
+      File.join(base_directory, clean_path)
     end
   end
 end
