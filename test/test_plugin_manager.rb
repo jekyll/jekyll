6a500226e5b468ebc75d8a40d18aee014c9a2a83
Parker Moore <parkrmoore@gmail.com>
2014-11-05 21:04:34 -0800

    Test JEKYLL_NO_BUNDLER_REQUIRE.

--- test/test_plugin_manager.rb
+++ test/test_plugin_manager.rb
@@ -2,7 +2,15 @@ require 'helper'
 
 class TestPluginManager < Test::Unit::TestCase
   def test_requiring_from_bundler
-    Jekyll::PluginManager.require_from_bundler
-    assert ENV["JEKYLL_NO_BUNDLER_REQUIRE"], 'Gemfile plugins were not required.'
+    with_env("JEKYLL_NO_BUNDLER_REQUIRE", nil) do
+      Jekyll::PluginManager.require_from_bundler
+      assert ENV["JEKYLL_NO_BUNDLER_REQUIRE"], 'Gemfile plugins were not required.'
+    end
+  end
+
+  def test_blocking_requiring_from_bundler
+    with_env("JEKYLL_NO_BUNDLER_REQUIRE", "true") do
+      assert_equal false, Jekyll::PluginManager.require_from_bundler, "Gemfile plugins were required but shouldn't have been"
+    end
   end
 end
