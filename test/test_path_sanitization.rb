c0650866f548cc77f2cb627a2e32709f2f84299d
Matt Robinson <git@nerdoftheherd.com>
2014-11-14 20:43:29 +0000

    Fix path case change in sanitized_path on Windows
    Under Windows the use of File.expand_path can change the case of the path
    being sanitized if there is a file or folder with the same name (but
    different case) in the root of the drive containing the working directory.
    
    This replaces the call to expand_path with regular expressions to achieve
    the same result without the side effects.
    
    Also add some extra tests to ensure that sanitize_path is working as
    intended and for the case change behaviour under Windows.

--- test/test_path_sanitization.rb
+++ test/test_path_sanitization.rb
@@ -1,18 +1,39 @@
 require 'helper'
 
 class TestPathSanitization < Test::Unit::TestCase
-  context "on Windows with absolute source" do
+  context "Path sanitization" do
     setup do
-      @source = "C:/Users/xmr/Desktop/mpc-hc.org"
-      @dest   = "./_site/"
-      stub(Dir).pwd { "C:/Users/xmr/Desktop/mpc-hc.org" }
-    end
-    should "strip drive name from path" do
-      assert_equal "C:/Users/xmr/Desktop/mpc-hc.org/_site", Jekyll.sanitized_path(@source, @dest)
+      @base_dir = "/tmp/foobar/jail"
     end
 
-    should "strip just the initial drive name" do
-      assert_equal "/tmp/foobar/jail/..c:/..c:/..c:/etc/passwd", Jekyll.sanitized_path("/tmp/foobar/jail", "..c:/..c:/..c:/etc/passwd")
+    should "remove single relative pair" do
+      assert_equal @base_dir + "/", Jekyll.sanitized_path(@base_dir, "css/..")
+    end
+
+    should "remove all dot path elements" do
+      assert_equal @base_dir + "/_posts/news", Jekyll.sanitized_path(@base_dir, "_posts/projects/../blog/../news")
+      assert_equal @base_dir + "/_posts", Jekyll.sanitized_path(@base_dir, "../_posts/./.")
+      assert_equal @base_dir + "/", Jekyll.sanitized_path(@base_dir, "..")
+      assert_equal @base_dir + "/_layouts/index.html", Jekyll.sanitized_path(@base_dir, "./_layouts/./index.html")
+    end
+
+    context "on Windows with absolute source" do
+      setup do
+        @source = "C:/Users/xmr/Desktop/mpc-hc.org"
+        @dest   = "./_site/"
+        stub(Dir).pwd { "C:/Users/xmr/Desktop/mpc-hc.org" }
+      end
+      should "strip drive name from path" do
+        assert_equal "C:/Users/xmr/Desktop/mpc-hc.org/_site", Jekyll.sanitized_path(@source, @dest)
+      end
+
+      should "not change path case" do
+        assert_equal @source + "/windows", Jekyll.sanitized_path(@source, "windows")
+      end
+
+      should "strip just the initial drive name" do
+        assert_equal "/tmp/foobar/jail/..c:/..c:/..c:/etc/passwd", Jekyll.sanitized_path("/tmp/foobar/jail", "..c:/..c:/..c:/etc/passwd")
+      end
     end
   end
 end
