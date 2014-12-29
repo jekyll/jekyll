7c8e4d46ebd09b2128371cdbcc02a44563220ed0
Parker Moore <parkrmoore@gmail.com>
2014-12-29 00:11:13 -0500

    Fix up new output for RDiscount 2.

--- test/test_rdiscount.rb
+++ test/test_rdiscount.rb
@@ -16,7 +16,24 @@ class TestRdiscount < Test::Unit::TestCase
     end
 
     should "render toc" do
-      assert_equal "<h1 id=\"Header+1\">Header 1</h1>\n\n<h2 id=\"Header+2\">Header 2</h2>\n\n<p>\n <ul>\n <li><a href=\"#Header+1\">Header 1</a>\n  <ul>\n  <li><a href=\"#Header+2\">Header 2</a>  </li>\n  </ul>\n </li>\n </ul>\n\n</p>", @markdown.convert("# Header 1\n\n## Header 2\n\n{:toc}").strip
+      toc = <<-TOC
+<a name="Header.1"></a>
+<h1>Header 1</h1>
+
+<a name="Header.2"></a>
+<h2>Header 2</h2>
+
+<p><ul>
+ <li><a href="#Header.1">Header 1</a>
+ <ul>
+  <li><a href="#Header.2">Header 2</a></li>
+ </ul>
+ </li>
+</ul>
+
+</p>
+TOC
+      assert_equal toc.strip, @markdown.convert("# Header 1\n\n## Header 2\n\n{:toc}").strip
     end
   end
 end
