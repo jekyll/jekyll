645a2cc664ca39c672c5139a294515cec10be0d8
Parker Moore <parkrmoore@gmail.com>
2015-12-14 20:59:52 -0800

    test/test_doctor_command.rb: fix test for Doctor.urls_only_differ_by_case

--- test/test_doctor_command.rb
+++ test/test_doctor_command.rb
@@ -13,7 +13,7 @@ class TestDoctorCommand < JekyllUnitTest
         "destination" => dest_dir
       }))
       @site.process
-      output = capture_stderr do 
+      output = capture_stderr do
          ret = Jekyll::Commands::Doctor.urls_only_differ_by_case(@site)
          assert_equal false, ret
       end
@@ -26,11 +26,11 @@ class TestDoctorCommand < JekyllUnitTest
         "destination" => dest_dir
       }))
       @site.process
-      output = capture_stderr do 
+      output = capture_stderr do
          ret = Jekyll::Commands::Doctor.urls_only_differ_by_case(@site)
          assert_equal true, ret
       end
-      assert_equal "\e[33m           Warning: The following URLs only differ by case. On a case-insensitive file system one of the URLs will be overwritten by the other: #{dest_dir}/about/index.html, #{dest_dir}/About/index.html\e[0m\n", output
+      assert_includes output, "Warning: The following URLs only differ by case. On a case-insensitive file system one of the URLs will be overwritten by the other: #{dest_dir}/about/index.html, #{dest_dir}/About/index.html"
     end
   end
 end
