f221b925b4fee632ec62653f3f9848e2d7763c22
Pat Hawks <pat@pathawks.com>
2016-01-03 17:16:58 -0800

    Rubocop: Lint/StringConversionInInterpolation  - Redundant use of Object#to_s in interpolation

--- lib/jekyll/tags/highlight.rb
+++ lib/jekyll/tags/highlight.rb
@@ -111,7 +111,7 @@ eos
       def add_code_tag(code)
         code_attributes = [
           "class=\"language-#{@lang.to_s.tr('+', '-')}\"",
-          "data-lang=\"#{@lang.to_s}\""
+          "data-lang=\"#{@lang}\""
         ].join(" ")
         "<figure class=\"highlight\"><pre><code #{code_attributes}>#{code.chomp}</code></pre></figure>"
       end
