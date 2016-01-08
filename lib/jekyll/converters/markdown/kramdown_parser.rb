8f8d7b8295ddf86ecf2e7921676c526613d2b10f
Jordon Bedwell <jordon@envygeeks.io>
2016-01-07 22:28:00 -0600

    Try again.

--- lib/jekyll/converters/markdown/kramdown_parser.rb
+++ lib/jekyll/converters/markdown/kramdown_parser.rb
@@ -15,10 +15,12 @@ module Jekyll
           "tab_width" => 4
         }
 
+        #
+
         def initialize(config)
+          @fallback = config["highlighter"] || "rogue"
           Jekyll::External.require_with_graceful_fail "kramdown"
-          @main_fallback_highlighter = config["highlighter"] || "rogue"
-          @config = Marshal.load(Marshal.dump(config["kramdown"] || {}))
+          @config = config["kramdown"] || {}
           setup
         end
 
@@ -29,17 +31,16 @@ module Jekyll
         #   * Make sure `syntax_highlighter_opts` exists.
 
         def setup
-          @config["syntax_highlighter"] ||= highlighter
           @config["syntax_highlighter_opts"] ||= {}
-          @config["coderay"] ||= {} # XXX: Legacy.
-          @coderay = @config.delete("coderay")
+          @config["syntax_highlighter"] ||= highlighter
+          @config["coderay"] ||= {}
           modernize_coderay_config
         end
 
         #
 
         def convert(content)
-          Kramdown::Document.new(content, @config.dup).to_html
+          Kramdown::Document.new(content, Marshal.load(Marshal.dump(@config))).to_html
         end
 
         # config[kramdown][syntax_higlighter] > config[kramdown][enable_coderay] > config[highlighter]
@@ -48,30 +49,12 @@ module Jekyll
 
         private
         def highlighter
-          @highlighter ||= begin
-            highlighter = @config["syntax_highlighter"]
-            return highlighter if highlighter
+          return @highlighter = @config["syntax_highlighter"] if @config["syntax_highlighter"]
+          return @highlighter = @fallback unless @config.key?("enable_coderay") && @config["enable_coderay"]
+          Jekyll::Deprecator.deprecation_message "You are using 'enable_coderay', use "\
+            "syntax_highlighter: coderay in your configuration file."
 
-            if @config.key?("enable_coderay") && @config["enable_coderay"]
-              Jekyll::Deprecator.deprecation_message "You are using 'enable_coderay', use "\
-                "syntax_highlighter: coderay in your configuration file."
-              return "coderay"
-            end
-
-            @main_fallback_highlighter
-          end
-        end
-
-        #
-
-        private
-        def strip_coderay_prefix(hash)
-          hash.each_with_object({}) do |(key, val), hsh|
-            cleaned_key = key.gsub(/\Acoderay_/, "")
-            Jekyll::Deprecator.deprecation_message "Kramdown: You are using '#{key}'. " \
-              "Normalizing to #{cleaned_key}." if key != cleaned_key
-            hsh[cleaned_key] = val
-          end
+          @highlighter = "coderay"
         end
 
         # If our highlighter is CodeRay we go in to merge the CodeRay defaults
@@ -82,14 +65,20 @@ module Jekyll
         def modernize_coderay_config
           if highlighter == "coderay"
             send_deprecation_message_if_using_old_opts
-            @config["syntax_highlighter_opts"] = \
-              begin
-                strip_coderay_prefix(
-                  CODERAY_DEFAULTS \
-                    .merge(@config["syntax_highlighter_opts"]) \
-                    .merge(@coderay)
-                )
-              end
+            opts = CODERAY_DEFAULTS.merge(@config["syntax_highlighter_opts"]).merge(@coderay)
+            @config["syntax_highlighter_opts"] = strip_coderay_prefix(opts)
+          end
+        end
+
+        #
+
+        private
+        def strip_coderay_prefix(hash)
+          hash.each_with_object({}) do |(key, val), hash_|
+            cleaned_key = key.to_s.gsub(/\Acoderay_/, "")
+            Jekyll::Deprecator.deprecation_message "Kramdown: You are using '#{key}'. " \
+              "Normalizing to #{cleaned_key}." if key != cleaned_key
+            hash_[cleaned_key] = val
           end
         end
 
