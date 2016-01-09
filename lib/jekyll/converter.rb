c13ee384094873e78049f9e31bf3f5c59c0191e4
Jordon Bedwell <jordon@envygeeks.io>
2016-01-09 05:27:01 -0600

    Reorganize Converter and start using Forwardable.

--- lib/jekyll/converter.rb
+++ lib/jekyll/converter.rb
@@ -1,48 +1,38 @@
 module Jekyll
   class Converter < Plugin
-    # Public: Get or set the highlighter prefix. When an argument is specified,
-    # the prefix will be set. If no argument is specified, the current prefix
-    # will be returned.
+    extend  Forwardable
+
     #
+
+    def_delegator :"self.class", :highlighter_prefix
+    def_delegator :"self.class", :highlighter_suffix
+
+    #
+
+    def initialize(config = {})
+      @config = config
+    end
+
     # highlighter_prefix - The String prefix (default: nil).
-    #
+    # Public: Get or set the highlighter prefix. When an argument is
+    # specified, the prefix will be set. If no argument is specified, the
+    # current prefix will be returned.
     # Returns the String prefix.
+
     def self.highlighter_prefix(highlighter_prefix = nil)
       @highlighter_prefix = highlighter_prefix if highlighter_prefix
       @highlighter_prefix
     end
 
-    # Public: Get or set the highlighter suffix. When an argument is specified,
-    # the suffix will be set. If no argument is specified, the current suffix
-    # will be returned.
-    #
     # highlighter_suffix - The String suffix (default: nil).
-    #
+    # Public: Get or set the highlighter suffix. When an argument is
+    # specified, the suffix will be set. If no argument is specified, the
+    # current suffix will be returned.
     # Returns the String suffix.
+
     def self.highlighter_suffix(highlighter_suffix = nil)
       @highlighter_suffix = highlighter_suffix if highlighter_suffix
       @highlighter_suffix
-    end
-
-    # Initialize the converter.
-    #
-    # Returns an initialized Converter.
-    def initialize(config = {})
-      @config = config
-    end
-
-    # Get the highlighter prefix.
-    #
-    # Returns the String prefix.
-    def highlighter_prefix
-      self.class.highlighter_prefix
-    end
-
-    # Get the highlighter suffix.
-    #
-    # Returns the String suffix.
-    def highlighter_suffix
-      self.class.highlighter_suffix
     end
   end
 end
