23d54dfebb5ad7b0dea94538241b5c77ba5679a7
Alfred Xing <alfredxing@users.noreply.github.com>
2016-01-31 11:11:50 -0800

    Allow path overrides in front-matter

--- lib/jekyll/drops/document_drop.rb
+++ lib/jekyll/drops/document_drop.rb
@@ -9,12 +9,16 @@ module Jekyll
 
       def_delegator :@obj, :next_doc, :next
       def_delegator :@obj, :previous_doc, :previous
-      def_delegator :@obj, :relative_path, :path
+      # def_delegator :@obj, :relative_path, :path
       def_delegators :@obj, :id, :output, :content, :to_s, :relative_path, :url
       def_delegators :@obj, :dir, :name
 
       def collection
         @obj.collection.label
+      end
+
+      def path
+        fallback_data['path'] || @obj.relative_path
       end
 
       def excerpt
