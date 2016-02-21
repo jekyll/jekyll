64ad293b899a66d6357cb320846b0804ae0d4518
Tim Wisniewski <tim@timwis.com>
2016-02-20 19:46:48 -0500

    add array support to where filter

--- lib/jekyll/filters.rb
+++ lib/jekyll/filters.rb
@@ -222,7 +222,7 @@ module Jekyll
     def where(input, property, value)
       return input unless input.is_a?(Enumerable)
       input = input.values if input.is_a?(Hash)
-      input.select { |object| item_property(object, property).to_s == value.to_s }
+      input.select { |object| item_property(object, property).to_s == value.to_s or item_property(object, property).include? value.to_s }
     end
 
     # Sort an array of objects
