59d4928f28b0fd348c66c2c4f8a329a496773431
Saleem Rashid <spudowiar@outlook.com>
2016-02-29 20:49:38 +0000

    layout_reader: allow 'layout_dirs' to be an array

--- test/test_layout_reader.rb
+++ test/test_layout_reader.rb
@@ -14,7 +14,7 @@ class TestLayoutReader < JekyllUnitTest
 
     context "when no _layouts directory exists in CWD" do
       should "know to use the layout directory relative to the site source" do
-        assert_equal LayoutReader.new(@site).layout_directory, source_dir("_layouts")
+        assert_equal LayoutReader.new(@site).layout_directories[0], source_dir("_layouts")
       end
     end
 
@@ -25,7 +25,31 @@ class TestLayoutReader < JekyllUnitTest
       end
 
       should "know to use the layout directory relative to CWD" do
-        assert_equal LayoutReader.new(@site).layout_directory, source_dir("blah/_layouts")
+        assert_equal LayoutReader.new(@site).layout_directories[0], source_dir("blah/_layouts")
+      end
+    end
+
+    context "when layout directories is a string" do
+      setup do
+        config = Jekyll::Configuration::DEFAULTS.merge({'layouts_dir' => "_layouts2"})
+        @site = fixture_site(config)
+      end
+
+      should "read layouts" do
+        layouts = LayoutReader.new(@site).read
+        assert_equal ["default2"], layouts.keys
+      end
+    end
+
+    context "when layout directories is an array" do
+      setup do
+        config = Jekyll::Configuration::DEFAULTS.merge({'layouts_dir' => ["_layouts2", "_layouts3"]})
+        @site = fixture_site(config)
+      end
+
+      should "read layouts" do
+        layouts = LayoutReader.new(@site).read
+        assert_equal ["default2", "default3"].sort, layouts.keys.sort
       end
     end
   end
