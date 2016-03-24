09f9f193d8e5fb714a6852c9276be1c6e55efbe8
Parker Moore <parkrmoore@gmail.com>
2016-03-23 17:15:35 -0700

    Add comment about github-pages

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -69,14 +69,18 @@ module Jekyll
 source 'https://rubygems.org'
   
 # Hello! This is where you manage which Jekyll version is used to run.
-# When you wwant to use a different version, change it below, save the
+# When you want to use a different version, change it below, save the
 # file and run `bundle install`. Run Jekyll with `bundle exec`, like so:
 #
 #     bundle exec jekyll serve
 #
 # This will help ensure the proper Jekyll version is running.
 # Happy Jekylling!
-gem 'jekyll', '#{Jekyll::VERSION}'
+gem "jekyll", "#{Jekyll::VERSION}"
+
+# If you want to use GitHub Pages, remove the "gem "jekyll"" above and
+# uncomment the line below. To upgrade, run `bundle update github-pages`.
+# gem "github-pages", group: :jekyll_plugins
   
 # If you have any plugins, put them here!
 # group :jekyll_plugins do
