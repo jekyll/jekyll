29d1f21df7c06ac58f575cd2731e327fb7d19ae2
Parker Moore <parkrmoore@gmail.com>
2016-03-31 13:48:00 -0700

    specs: use subject

--- spec/jekyll/configuration_spec.rb
+++ spec/jekyll/configuration_spec.rb
@@ -1,10 +1,10 @@
 RSpec.describe(Jekyll::Configuration) do
-  let(:config) { described_class.from({}) }
+  subject { described_class.from({}) }
 
   describe "#safe_load_file" do
     it "throws an InvalidFileFormat on a bad extension" do
       filename = "my_config.json"
-      expect(-> { config.safe_load_file(filename) }).to raise_error(
+      expect(-> { subject.safe_load_file(filename) }).to raise_error(
         Jekyll::Errors::InvalidFileFormatError,
         "No parser for '#{filename}' is available. Use a .toml or .y(a)ml file instead."
       )
@@ -14,7 +14,7 @@ RSpec.describe(Jekyll::Configuration) do
   describe "#read_config_file" do
     it "throws a FileNotFoundError if the file doesn't exist" do
       filename = "_config.yml"
-      expect(-> { config.read_config_file(filename) }).to raise_error(
+      expect(-> { subject.read_config_file(filename) }).to raise_error(
         Jekyll::Errors::FileNotFoundError,
         "The configuration file '#{filename}' could not be found."
       )
@@ -23,12 +23,12 @@ RSpec.describe(Jekyll::Configuration) do
 
   describe "#check_config_is_hash!" do
     it "does not throw if the input is a hash" do
-      expect(config.send(:check_config_is_hash!, {}, "_config.yml")).to eq(nil)
+      expect(subject.send(:check_config_is_hash!, {}, "_config.yml")).to eq(nil)
     end
 
     it "throws an InvalidConfigurationError if the input is not a hash" do
       filename = "_config.yml"
-      expect(-> { config.send(:check_config_is_hash!, false, filename) }).to raise_error(
+      expect(-> { subject.send(:check_config_is_hash!, false, filename) }).to raise_error(
         Jekyll::Errors::InvalidConfigurationError,
         "The configuration file '#{filename}' is invalid: it is not a Hash."
       )
