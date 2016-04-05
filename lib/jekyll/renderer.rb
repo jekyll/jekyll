34c0de694494ac4593df3344e840d0a8d795ae2b
John Hughes <john@castlegateit.co.uk>
2016-04-05 08:26:00 +0100

    Rename rendered content hook

--- lib/jekyll/renderer.rb
+++ lib/jekyll/renderer.rb
@@ -67,7 +67,7 @@ module Jekyll
       document.content = output
 
       # Hook to modify converted content before layout is applied
-      document.trigger_hooks(:post_content_render, output)
+      document.trigger_hooks(:post_render_content, output)
       output = document.content
 
       if document.place_in_layout?
