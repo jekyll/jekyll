6f7ac363e60b64d501ce1e165f1722ed3cbd7fbe
John Hughes <john@castlegateit.co.uk>
2016-04-05 08:26:00 +0100

    Rename rendered content hook

--- lib/jekyll/hooks.rb
+++ lib/jekyll/hooks.rb
@@ -22,21 +22,21 @@ module Jekyll
       :pages => {
         :post_init => [],
         :pre_render => [],
-        :post_content_render => [],
+        :post_render_content => [],
         :post_render => [],
         :post_write => []
       },
       :posts => {
         :post_init => [],
         :pre_render => [],
-        :post_content_render => [],
+        :post_render_content => [],
         :post_render => [],
         :post_write => []
       },
       :documents => {
         :post_init => [],
         :pre_render => [],
-        :post_content_render => [],
+        :post_render_content => [],
         :post_render => [],
         :post_write => []
       }
