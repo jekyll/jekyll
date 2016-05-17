b006810f172a94e9bd5aa9cad8440e5cd1bacac1
Brint O'Hearn <brint.ohearn@rackspace.com>
2016-05-16 19:59:13 -0500

    Moving .with down one line 4x times

--- test/test_log_adapter.rb
+++ test/test_log_adapter.rb
@@ -61,8 +61,8 @@ class TestLogAdapter < JekyllUnitTest
     should "call #debug on writer return true" do
       writer = LoggerDouble.new
       logger = Jekyll::LogAdapter.new(writer)
-      allow(writer).to receive(:debug).with("topic ".rjust(20) +
-                                            "log message").and_return(true)
+      allow(writer).to receive(:debug)
+        .with("topic ".rjust(20) + "log message").and_return(true)
       assert logger.debug("topic", "log message")
     end
   end
@@ -71,8 +71,8 @@ class TestLogAdapter < JekyllUnitTest
     should "call #info on writer return true" do
       writer = LoggerDouble.new
       logger = Jekyll::LogAdapter.new(writer)
-      allow(writer).to receive(:info).with("topic ".rjust(20) +
-                                           "log message").and_return(true)
+      allow(writer).to receive(:info)
+        .with("topic ".rjust(20) + "log message").and_return(true)
       assert logger.info("topic", "log message")
     end
   end
@@ -81,8 +81,8 @@ class TestLogAdapter < JekyllUnitTest
     should "call #warn on writer return true" do
       writer = LoggerDouble.new
       logger = Jekyll::LogAdapter.new(writer)
-      allow(writer).to receive(:warn).with("topic ".rjust(20) +
-                                           "log message").and_return(true)
+      allow(writer).to receive(:warn)
+        .with("topic ".rjust(20) + "log message").and_return(true)
       assert logger.warn("topic", "log message")
     end
   end
@@ -91,8 +91,8 @@ class TestLogAdapter < JekyllUnitTest
     should "call #error on writer return true" do
       writer = LoggerDouble.new
       logger = Jekyll::LogAdapter.new(writer)
-      allow(writer).to receive(:error).with("topic ".rjust(20) +
-                                            "log message").and_return(true)
+      allow(writer).to receive(:error)
+        .with("topic ".rjust(20) + "log message").and_return(true)
       assert logger.error("topic", "log message")
     end
   end
