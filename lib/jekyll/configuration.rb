8fae81861d59c96bf9d6e154c932c3966162f557
Parker Moore <parkrmoore@gmail.com>
2016-05-18 12:49:23 -0700

    Don't default 'include' and 'exclude' to an empty array

--- lib/jekyll/configuration.rb
+++ lib/jekyll/configuration.rb
@@ -245,7 +245,6 @@ module Jekyll
       end
 
       %w(include exclude).each do |option|
-        config[option] ||= []
         if config[option].is_a?(String)
           Jekyll::Deprecator.deprecation_message "The '#{option}' configuration option" \
             " must now be specified as an array, but you specified" \
