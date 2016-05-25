2caf8e06213eb0812cb17384bac8c3796c6ae610
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2016-05-25 21:48:21 +0300

    rubocop: fix spacing in code style

--- lib/jekyll/tags/highlight.rb
+++ lib/jekyll/tags/highlight.rb
@@ -112,7 +112,7 @@ eos
         Jekyll::External.require_with_graceful_fail("rouge")
         formatter = Rouge::Formatters::HTML.new(
           :line_numbers => @highlight_options[:linenos],
-          :wrap => false
+          :wrap         => false
         )
         lexer = Rouge::Lexer.find_fancy(@lang, code) || Rouge::Lexers::PlainText
         formatter.format(lexer.lex(code))
