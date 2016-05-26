382be191aed931174e19da4f9edd000665532933
Pat Hawks <pat@pathawks.com>
2016-05-26 00:10:43 -0500

    Rubocop: lib/jekyll/readers/static_file_reader.rb

--- lib/jekyll/readers/static_file_reader.rb
+++ lib/jekyll/readers/static_file_reader.rb
@@ -14,7 +14,9 @@ module Jekyll
     #
     # Returns an array of static files.
     def read(files)
-      files.map { |file| @unfiltered_content << StaticFile.new(@site, @site.source, @dir, file) }
+      files.map do |file|
+        @unfiltered_content << StaticFile.new(@site, @site.source, @dir, file)
+      end
       @unfiltered_content
     end
   end
