615a9ee9ac971668f203db76955a247b08926509
Pat Hawks <pat@pathawks.com>
2016-05-26 00:10:20 -0500

    Rubocop: lib/jekyll/readers/page_reader.rb

--- lib/jekyll/readers/page_reader.rb
+++ lib/jekyll/readers/page_reader.rb
@@ -14,7 +14,9 @@ module Jekyll
     #
     # Returns an array of static pages.
     def read(files)
-      files.map { |page| @unfiltered_content << Page.new(@site, @site.source, @dir, page) }
+      files.map do |page|
+        @unfiltered_content << Page.new(@site, @site.source, @dir, page)
+      end
       @unfiltered_content.select { |page| site.publisher.publish?(page) }
     end
   end
