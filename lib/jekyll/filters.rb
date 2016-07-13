5194d1a42ccfceb2747a03c06e0b7ac7912f44b9
David Zhang <crispgm@gmail.com>
2016-07-13 10:52:18 +0800

    Add to_integer filter

--- lib/jekyll/filters.rb
+++ lib/jekyll/filters.rb
@@ -260,6 +260,15 @@ module Jekyll
       end
     end
 
+    # Convert the input into integer
+    #
+    # input - the object string
+    #
+    # Returns the integer value
+    def to_integer(input)
+      input.to_i
+    end
+
     # Sort an array of objects
     #
     # input - the object array
