0d8796fbe60dbca7e7af8d82bf94a822a8ffce90
Heng, K. (Stephen) <kwokfu.heng@gmail.com>
2016-08-26 11:20:03 +0800

    Test case for issue #5276, where Jekyll.sanitized_path strips base path incorrectly if file path has matching prefix

--- test/test_path_sanitization.rb
+++ test/test_path_sanitization.rb
@@ -28,4 +28,22 @@ class TestPathSanitization < JekyllUnitTest
     assert_equal source_dir("files", "hi.txt"),
                  Jekyll.sanitized_path(source_dir, "f./../../../../../../files/hi.txt")
   end
+
+  context "on Windows with file path has matching prefix" do
+    setup do
+      @base_path = "D:/site"
+      @file_path = "D:/sitemap.xml"
+      allow(Dir).to receive(:pwd).and_return("D:/")
+    end
+
+    should "not strip base path" do
+      assert_equal "D:/site/sitemap.xml",
+                   Jekyll.sanitized_path(@base_path, @file_path)
+    end
+  end
+
+  should "not strip base path if file path has matching prefix" do
+    assert_equal "/site/sitemap.xml",
+                 Jekyll.sanitized_path("/site", "sitemap.xml")
+  end
 end
