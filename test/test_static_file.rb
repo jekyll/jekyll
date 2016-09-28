fd5f2eb39932f313e5264f6c79b1c073aa7162d4
Parker Moore <parkrmoore@gmail.com>
2016-09-28 16:54:58 -0700

    Add failing test for File.utime of a symlink in staticfile.

--- test/test_static_file.rb
+++ test/test_static_file.rb
@@ -112,6 +112,14 @@ class TestStaticFile < JekyllUnitTest
       assert_equal Time.new.to_i, @static_file.mtime
     end
 
+    should "only set modified time if not a symlink" do
+      expect(File).to receive(:symlink?).and_return(true)
+      expect(File).not_to receive(:utime)
+      @static_file.write(dest_dir)
+
+      allow(File).to receive(:symlink?).and_call_original
+    end
+
     should "known if the source path is modified, when it is" do
       sleep 1
       modify_dummy_file(@filename)
