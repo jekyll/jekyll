63e4e750ec47b4dc1d6fbe717da47f354d9b56fc
Parker Moore <parkrmoore@gmail.com>
2016-10-06 09:58:16 -0700

    New site: lock minima to v2.x

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -74,7 +74,7 @@ ruby RUBY_VERSION
 gem "jekyll", "#{Jekyll::VERSION}"
 
 # This is the default theme for new Jekyll sites. You may change this to anything you like.
-gem "minima"
+gem "minima", "~> 2.0"
 
 # If you want to use GitHub Pages, remove the "gem "jekyll"" above and
 # uncomment the line below. To upgrade, run `bundle update github-pages`.
