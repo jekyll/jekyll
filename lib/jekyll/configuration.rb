b194211a32f3da646bf96ef58d4081ea3a2da130
Parker Moore <parkrmoore@gmail.com>
2016-11-09 08:15:55 -0800

    Be much more specific about ignoring vendored directories.

--- lib/jekyll/configuration.rb
+++ lib/jekyll/configuration.rb
@@ -17,7 +17,7 @@ module Jekyll
       # Handling Reading
       "safe"              => false,
       "include"           => [".htaccess"],
-      "exclude"           => %w(node_modules vendor),
+      "exclude"           => %w(node_modules vendor/bundle vendor/ruby vendor/cache),
       "keep_files"        => [".git", ".svn"],
       "encoding"          => "utf-8",
       "markdown_ext"      => "markdown,mkdown,mkdn,mkd,md",
