899021777970446be1ae9749d3fe601d8a6edcea
Martin Desrumaux <martin.desrumaux@epitech.eu>
2016-12-03 01:24:00 +0100

    Fix accessing subvalue from filters

--- lib/jekyll/filters.rb
+++ lib/jekyll/filters.rb
@@ -389,7 +389,11 @@ module Jekyll
     private
     def item_property(item, property)
       if item.respond_to?(:to_liquid)
-        item.to_liquid[property.to_s]
+        res = item.to_liquid
+        for attr in property.to_s.split(".")
+          res = res[attr]
+        end
+        res
       elsif item.respond_to?(:data)
         item.data[property.to_s]
       else
