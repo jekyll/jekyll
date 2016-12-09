d4c8d7fd2be52a75a805ed920f38c38cb3611e3d
Thiago Arrais <thiago.arrais@gmail.com>
2016-12-09 09:56:40 -0300

    Ignore symlinked file in windows

--- test/test_filters.rb
+++ test/test_filters.rb
@@ -762,7 +762,9 @@ class TestFilters < JekyllUnitTest
               g["items"].is_a?(Array),
               "The list of grouped items for 'default' is not an Array."
             )
-            assert_equal 5, g["items"].size
+            # adjust array.size to ignore symlinked page in Windows
+            qty = Utils::Platforms.really_windows? ? 4 : 5
+            assert_equal qty, g["items"].size
           when "nil"
             assert(
               g["items"].is_a?(Array),
@@ -774,7 +776,9 @@ class TestFilters < JekyllUnitTest
               g["items"].is_a?(Array),
               "The list of grouped items for '' is not an Array."
             )
-            assert_equal 15, g["items"].size
+            # adjust array.size to ignore symlinked page in Windows
+            qty = Utils::Platforms.really_windows? ? 14 : 15
+            assert_equal qty, g["items"].size
           end
         end
       end
