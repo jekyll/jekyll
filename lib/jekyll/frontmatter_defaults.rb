dfe80170f0134623ea0a2fca440bd7a75531c2bd
Alexey Pelykh <alexey.pelykh@gmail.com>
2017-03-31 09:20:18 +0300

    Fix non-existend files matching

--- lib/jekyll/frontmatter_defaults.rb
+++ lib/jekyll/frontmatter_defaults.rb
@@ -100,17 +100,26 @@ module Jekyll
     def applies_path?(scope, path)
       return true if !scope.key?("path") || scope["path"].empty?
 
+      sanitized_path = sanitize_path(path)
+
       site_path = Pathname.new(@site.source)
       rel_scope_path = Pathname.new(scope["path"])
       abs_scope_path = File.join(@site.source, rel_scope_path)
       Dir.glob(abs_scope_path).each do |scope_path|
         scope_path = Pathname.new(scope_path).relative_path_from site_path
-        Pathname.new(sanitize_path(path)).ascend do |ascended_path|
+        Pathname.new(sanitized_path).ascend do |ascended_path|
           if ascended_path.to_s == scope_path.to_s
             return true
           end
         end
       end
+
+      Pathname.new(sanitized_path).ascend do |ascended_path|
+        if ascended_path.to_s == rel_scope_path.to_s
+          return true
+        end
+      end
+
       return false
     end
 
