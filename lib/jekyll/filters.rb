213939070413adaeadfe98f95ad2b973712202dc
Martin Desrumaux <martin.desrumaux@epitech.eu>
2017-05-17 13:39:43 +0200

    Rubocop fix && Added subproperties for data files

--- lib/jekyll/filters.rb
+++ lib/jekyll/filters.rb
@@ -361,9 +361,13 @@ module Jekyll
     private
     def item_property(item, property)
       if item.respond_to?(:to_liquid)
-        property.to_s.split(".").reduce(item.to_liquid) { |val, attr| val[attr] }
+        property.to_s.split(".").reduce(item.to_liquid) do |subvalue, attribute|
+          subvalue[attribute]
+        end
       elsif item.respond_to?(:data)
-        item.data[property.to_s]
+        property.to_s.split(".").reduce(item.data) do |subvalue, attribute|
+          subvalue[attribute]
+        end
       else
         item[property.to_s]
       end
