63a96034ddd16bae48085c8009860a8a8e926ada
Ben Balter <ben.balter@github.com>
2017-06-20 11:02:57 -0400

    failing test for static files not having front matter defaults

--- test/test_static_file.rb
+++ test/test_static_file.rb
@@ -109,6 +109,28 @@ class TestStaticFile < JekyllUnitTest
         "`published: false`")
     end
 
+    should "respect front matter defaults" do
+      defaults = [{
+        "scope"  => { "path" => "" },
+        "values" => { "front-matter" => "default" },
+      },]
+
+      static_file = setup_static_file_with_defaults "", "", "file.pdf", defaults
+      assert_equal "default", static_file.data["front-matter"]
+    end
+
+    should "include front matter defaults in to_liquid" do
+      defaults = [{
+        "scope"  => { "path" => "" },
+        "values" => { "front-matter" => "default" },
+      },]
+
+      static_file = setup_static_file_with_defaults "", "", "file.pdf", defaults
+      hash = static_file.to_liquid
+      assert hash.key? "front-matter"
+      assert_equal "default", hash["front-matter"]
+    end
+
     should "know its last modification time" do
       assert_equal Time.new.to_i, @static_file.mtime
     end
