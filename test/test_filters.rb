9f7815786b68f853dfabb33293f494e4222c5c1b
Ben Balter <282759+benbalter@users.noreply.github.com>
2017-06-30 21:58:25 -0400

    Always normalize the result of the relative_url filter (#6185)
    Merge pull request 6185

--- test/test_filters.rb
+++ test/test_filters.rb
@@ -491,6 +491,15 @@ class TestFilters < JekyllUnitTest
         })
         assert_equal "/", filter.relative_url(page_url)
       end
+
+      should "not return the url by reference" do
+        filter = make_filter_mock({ :baseurl => nil })
+        page = Page.new(filter.site, test_dir("fixtures"), "", "front_matter.erb")
+        assert_equal "/front_matter.erb", page.url
+        url = filter.relative_url(page.url)
+        url << "foo"
+        assert_equal "/front_matter.erb", page.url
+      end
     end
 
     context "strip_index filter" do
