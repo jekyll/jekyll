9728d84647a0c556f9259516d533afe4bc8c0699
Jordon Bedwell <jordon@envygeeks.io>
2017-07-02 21:47:01 -0500

    Github-Pages is not a plugin, it's a replacement.
    Putting Github-Pages inside of Jekyll Plugins is bad form, it's also misleading to users who often assume it's a plugin, it's not... it's a replacement for Jekyll in it's entirety, with it's own patch set.

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -76,8 +76,8 @@ gem "jekyll", "#{Jekyll::VERSION}"
 gem "minima", "~> 2.0"
 
 # If you want to use GitHub Pages, remove the "gem "jekyll"" above and
-# uncomment the line below. To upgrade, run `bundle update github-pages`.
-# gem "github-pages", group: :jekyll_plugins
+# uncomment the line below. To upgrade, run `bundle update`.
+# gem "github-pages"
 
 # If you have any plugins, put them here!
 group :jekyll_plugins do
