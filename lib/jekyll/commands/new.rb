e25c4af59066169d0e10056866759911198087c6
Jordon Bedwell <jordon@envygeeks.io>
2017-07-24 05:02:24 -0500

    Users should be installing patch versions. (#6198)
    Merge pull request 6198

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -70,7 +70,7 @@ source "https://rubygems.org"
 #
 # This will help ensure the proper Jekyll version is running.
 # Happy Jekylling!
-gem "jekyll", "#{Jekyll::VERSION}"
+gem "jekyll", "~> #{Jekyll::VERSION}"
 
 # This is the default theme for new Jekyll sites. You may change this to anything you like.
 gem "minima", "~> 2.0"
