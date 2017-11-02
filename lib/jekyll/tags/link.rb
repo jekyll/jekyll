a822d73d293bf2bc7a8cd2b51405f5e37d103a28
Ashwin Maroli <ashmaroli@gmail.com>
2017-11-02 22:15:36 +0530

    refactor Tags::Link

--- lib/jekyll/tags/link.rb
+++ lib/jekyll/tags/link.rb
@@ -3,6 +3,7 @@
 module Jekyll
   module Tags
     class Link < Liquid::Tag
+      # TODO: Remove singleton class in v4.0
       class << self
         def tag_name
           self.name.split("::").last.downcase
@@ -11,21 +12,21 @@ module Jekyll
 
       def initialize(tag_name, relative_path, tokens)
         super
-
         @relative_path = relative_path.strip
       end
 
       def render(context)
+        # additionally handle static files, that have a leading `/`
+        valid_paths = [@relative_path, "/#{@relative_path}"]
         site = context.registers[:site]
 
         site.each_site_file do |item|
-          return item.url if item.relative_path == @relative_path
-          # This takes care of the case for static files that have a leading /
-          return item.url if item.relative_path == "/#{@relative_path}"
+          rel_path = item.relative_path
+          return item.url if valid_paths.include?(rel_path)
         end
 
         raise ArgumentError, <<-MSG
-Could not find document '#{@relative_path}' in tag '#{self.class.tag_name}'.
+Could not find document '#{@relative_path}' in tag 'link'.
 
 Make sure the document exists and the path is correct.
 MSG
@@ -34,4 +35,4 @@ MSG
   end
 end
 
-Liquid::Template.register_tag(Jekyll::Tags::Link.tag_name, Jekyll::Tags::Link)
+Liquid::Template.register_tag("link", Jekyll::Tags::Link)
