8081f5493693e33e59e72a7263e0d834169b202b
ashmaroli <ashmaroli@users.noreply.github.com>
2017-11-10 18:55:01 +0530

    use double-quotes around gem name (#6535)
    Merge pull request 6535

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -87,7 +87,7 @@ group :jekyll_plugins do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 RUBY
         end
