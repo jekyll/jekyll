3f51aba22e49fd9ab82009eb583e14adac6b9324
Pat Hawks <pat@pathawks.com>
2017-11-16 20:02:43 -0600

    Add test for multi-line SmartyPants

--- test/test_filters.rb
+++ test/test_filters.rb
@@ -100,6 +100,10 @@ class TestFilters < JekyllUnitTest
         assert_equal "“", @filter.smartify("&ldquo;")
       end
 
+      should "convert multiple lines" do
+        assert_equal "…\n…", @filter.smartify("...\n...")
+      end
+
       should "allow raw HTML passthrough" do
         assert_equal(
           "Span HTML is <em>not</em> escaped",
