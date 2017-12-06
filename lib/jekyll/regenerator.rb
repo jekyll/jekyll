28aa835e1e5a4bbeca1bc1dc2defe0f644af17df
Ashwin Maroli <ashmaroli@gmail.com>
2017-12-06 18:32:07 +0530

    check theme-files only if site uses a theme-gem

--- lib/jekyll/regenerator.rb
+++ lib/jekyll/regenerator.rb
@@ -26,7 +26,7 @@ module Jekyll
       when Document
         regenerate_document?(document)
       else
-        return false if document.start_with?(theme_root)
+        return false if document.is_a?(String) && file_in_theme_dir?(document)
         source_path = document.respond_to?(:path) ? document.path : nil
         dest_path = if document.respond_to?(:destination)
                       document.destination(@site.dest)
@@ -89,7 +89,7 @@ module Jekyll
       return true if path.nil?
 
       # objects from theme-gem are never regenerated
-      return false if path.start_with?(theme_root)
+      return false if file_in_theme_dir?(path)
 
       # Check for path in cache
       if cache.key? path
@@ -205,6 +205,12 @@ module Jekyll
     private
     def theme_root
       @theme_root ||= site.theme.root
+    end
+
+    private
+    def file_in_theme_dir?(path)
+      return false unless site.theme
+      path.start_with?(theme_root)
     end
   end
 end
