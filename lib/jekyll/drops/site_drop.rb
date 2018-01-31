3dc0c7b5cb582efd43aafe5945b31db15fce338c
Parker Moore <parkrmoore@gmail.com>
2018-01-30 21:31:22 -0500

    Do not assign site.related_posts unless it's requested.
    Instead, assign the current document in the SiteDrop and generate the related_posts if requested by Liquid.

--- lib/jekyll/drops/site_drop.rb
+++ lib/jekyll/drops/site_drop.rb
@@ -37,6 +37,16 @@ module Jekyll
         @site_collections ||= @obj.collections.values.sort_by(&:label).map(&:to_liquid)
       end
 
+      # `{{ site.related_posts }}` is how posts can get posts related to
+      # them, either through LSI if it's enabled, or through the most
+      # recent posts.
+      # We should remove this in 4.0 and switch to `{{ post.related_posts }}`.
+      def related_posts
+        return nil unless @current_document.is_a?(Jekyll::Document)
+        @current_document.related_posts
+      end
+      attr_writer :current_document
+
       # return nil for `{{ site.config }}` even if --config was passed via CLI
       def config; end
 
