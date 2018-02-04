745aff32d8ee439e8550ee9d5999646e1fa14b8b
Ashwin Maroli <ashmaroli@gmail.com>
2018-02-04 15:58:10 +0530

    fix test for excerpt path in drop

--- test/test_excerpt.rb
+++ test/test_excerpt.rb
@@ -86,7 +86,7 @@ class TestExcerpt < JekyllUnitTest
         assert_equal Time.parse("2013-07-22"), @excerpt.to_liquid["date"]
         assert_equal %w(bar baz z_category MixedCase), @excerpt.to_liquid["categories"]
         assert_equal %w(first second third jekyllrb.com), @excerpt.to_liquid["tags"]
-        assert_equal "_posts/2013-07-22-post-excerpt-with-layout.markdown",
+        assert_equal "_posts/2013-07-22-post-excerpt-with-layout.markdown/#excerpt",
                      @excerpt.to_liquid["path"]
       end
     end
