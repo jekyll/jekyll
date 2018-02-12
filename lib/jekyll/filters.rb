63b7157d68dc8726e48ee31078c2fc1591fc9b2f
Ana María Martínez Gómez <ammartinez@suse.de>
2018-02-12 17:02:48 +0100

    Add ordinal_date filter
    It is normal to write things like _posted on 12th February 2018" in the
    blog post. Currently to achieve this, a code similar to this one is
    needed:
    
    ```
    {% assign d = page.date | date: "%-d" %}
    {% case d %}
    {% when '1' or '21' or '31' %}
    {{ d }}st
    {% when '2' or '22' %}
    {{ d }}nd
    {% when '3' or '23' %}
    {{ d }}rd
    {% else %}{{ d }}th
    {% endcase %}
    {{ page.date | date: "%B" }}
    {{ page.date | date: "%Y" }}
    ```
    
    I think the case is common enough to introduce it in Jekyll, by adding
    some parameters to the `date_to_string` and `date_to_long_string`
    methods.
    
    It supports both UK and US styles. UK is the default.

--- lib/jekyll/filters.rb
+++ lib/jekyll/filters.rb
@@ -68,22 +68,33 @@ module Jekyll
     end
 
     # Format a date in short format e.g. "27 Jan 2011".
+    # Ordinal format is also supported, in both the UK
+    # (e.g. "27th Jan 2011") and US ("e.g. Jan 27th, 2011") formats.
+    # UK format is the default.
     #
     # date - the Time to format.
+    # type - if "ordinal" the returned String will be in ordinal format
+    # style - if "US" the returned String will be in US format.
+    #         Otherwise it will be in UK format.
     #
     # Returns the formatting String.
-    def date_to_string(date)
-      time(date).strftime("%d %b %Y")
+    def date_to_string(date, type = nil, style = nil)
+      stringify_date(date, "%b", type, style)
     end
 
     # Format a date in long format e.g. "27 January 2011".
+    # Ordinal format is also supported, in both the UK
+    # (e.g. "27th January 2011") and US ("e.g. January 27th, 2011") formats.
+    # UK format is the default.
     #
-    # date - The Time to format.
+    # date - the Time to format.
+    # type - if "ordinal" the returned String will be in ordinal format
+    # style - if "US" the returned String will be in US format.
+    #         Otherwise it will be in UK format.
     #
     # Returns the formatted String.
-    def date_to_long_string(date)
-      return date if date.to_s.empty?
-      time(date).strftime("%d %B %Y")
+    def date_to_long_string(date, type = nil, style = nil)
+      stringify_date(date, "%B", type, style)
     end
 
     # Format a date for use in XML.
@@ -355,6 +366,36 @@ module Jekyll
           end
         end
         .map!(&:last)
+    end
+
+    private
+    # month_type: Notations that evaluate to 'Month' via `Time#strftime` ("%b", "%B")
+    # type: nil (default) or "ordinal"
+    # style: nil (default) or "US"
+    #
+    # Returns a stringified date or the empty input.
+    def stringify_date(date, month_type, type = nil, style = nil)
+      return date if date.to_s.empty?
+      time = time(date)
+      if type == "ordinal"
+        day = time.day
+        ordinal_day = "#{day}#{ordinal(day)}"
+        return time.strftime("#{month_type} #{ordinal_day}, %Y") if style == "US"
+        return time.strftime("#{ordinal_day} #{month_type} %Y")
+      end
+      time.strftime("%d #{month_type} %Y")
+    end
+
+    private
+    def ordinal(number)
+      return "th" if (11..13).cover?(number)
+
+      case number % 10
+      when 1 then "st"
+      when 2 then "nd"
+      when 3 then "rd"
+      else "th"
+      end
     end
 
     private
