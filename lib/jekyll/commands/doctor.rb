cef66defbd1c6d2a3e49272a5a2d6a0a86738bb9
ashmaroli <ashmaroli@users.noreply.github.com>
2018-02-20 20:12:45 +0530

    Allow Jekyll Doctor to detect stray posts dir (#6681)
    Merge pull request 6681

--- lib/jekyll/commands/doctor.rb
+++ lib/jekyll/commands/doctor.rb
@@ -41,7 +41,20 @@ module Jekyll
             !conflicting_urls(site),
             !urls_only_differ_by_case(site),
             proper_site_url?(site),
+            properly_gathered_posts?(site),
           ].all?
+        end
+
+        def properly_gathered_posts?(site)
+          return true if site.config["collections_dir"].empty?
+          posts_at_root = site.in_source_dir("_posts")
+          return true unless File.directory?(posts_at_root)
+          Jekyll.logger.warn "Warning:",
+            "Detected '_posts' directory outside custom `collections_dir`!"
+          Jekyll.logger.warn "",
+            "Please move '#{posts_at_root}' into the custom directory at " \
+            "'#{site.in_source_dir(site.config["collections_dir"])}'"
+          false
         end
 
         def deprecated_relative_permalinks(site)
