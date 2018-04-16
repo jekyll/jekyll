e2285e5b479c0f98fb9e2931b6ab45cabffa3586
Ashwin Maroli <ashmaroli@gmail.com>
2018-04-16 23:04:08 +0530

    move regex to a constant

--- lib/jekyll/tags/highlight.rb
+++ lib/jekyll/tags/highlight.rb
@@ -65,12 +65,14 @@ MSG
 
       private
 
+      OPTIONS_REGEX = %r!(?:\w="[^"]*"|\w=\w|\w)+!
+
       def parse_options(input)
         options = {}
         return options if input.empty?
 
         # Split along 3 possible forms -- key="<quoted list>", key=value, or key
-        input.scan(%r!(?:\w="[^"]*"|\w=\w|\w)+!) do |opt|
+        input.scan(OPTIONS_REGEX) do |opt|
           key, value = opt.split("=")
           # If a quoted list, convert to array
           if value && value.include?('"')
