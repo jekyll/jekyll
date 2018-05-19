ee87d6f0f7d8d06b72746e34bc6910b40ef6c03c
Philip Belesky <contact@philipbelesky.com>
2018-05-19 17:48:49 +1000

    Fix --unpublished not affecting collection documents
    As described in Issue #7026

--- lib/jekyll/collection.rb
+++ lib/jekyll/collection.rb
@@ -212,7 +212,9 @@ module Jekyll
     def read_document(full_path)
       doc = Document.new(full_path, :site => site, :collection => self)
       doc.read
-      docs << doc unless doc.data["published"] == false
+      unless doc.data["published"] == false && !site.unpublished
+        docs << doc
+      end
     end
 
     private
