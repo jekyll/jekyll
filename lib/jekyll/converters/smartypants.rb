6684adc7ea83b839cd2af16083a2464c673d54e0
Frank Taillandier <frank.taillandier@gmail.com>
2018-05-31 16:43:31 +0200

    Enable Style/ClassAndModuleChildren

--- lib/jekyll/converters/smartypants.rb
+++ lib/jekyll/converters/smartypants.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
-class Kramdown::Parser::SmartyPants < Kramdown::Parser::Kramdown
-  def initialize(source, options)
-    super
-    @block_parsers = [:block_html, :content]
-    @span_parsers =  [:smart_quotes, :html_entity, :typographic_syms, :span_html]
-  end
+module Kramdown
+  module Parser
+    class SmartyPants < Kramdown::Parser::Kramdown
+      def initialize(source, options)
+        super
+        @block_parsers = [:block_html, :content]
+        @span_parsers =  [:smart_quotes, :html_entity, :typographic_syms, :span_html]
+      end
 
-  def parse_content
-    add_text @src.scan(%r!\A.*\n!)
+      def parse_content
+        add_text @src.scan(%r!\A.*\n!)
+      end
+      define_parser(:content, %r!\A!)
+    end
   end
-  define_parser(:content, %r!\A!)
 end
 
 module Jekyll
