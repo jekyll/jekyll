94fb5985b7a2c462e20d036f39733ad5267fc274
Pat Hawks <pat@pathawks.com>
2018-07-01 21:16:48 -0500

    Fix bugs

--- lib/jekyll/page_without_a_file.rb
+++ lib/jekyll/page_without_a_file.rb
@@ -14,5 +14,21 @@ module Jekyll
     def inspect
       "#<Jekyll:PageWithoutAFile @name=#{name.inspect}>"
     end
+
+    def draft?
+      !!data["draft"]
+    end
+
+    def do_nothing(*)
+      nil
+    end
+    alias_method :process, :do_nothing
+    alias_method :read, :do_nothing
+    alias_method :read_content, :do_nothing
+    alias_method :read_post_data, :do_nothing
+    alias_method :categories_from_path, :do_nothing
+    alias_method :populate_categories, :do_nothing
+    alias_method :populate_tags, :do_nothing
+    alias_method :backwards_compatibilize, :do_nothing
   end
 end
