a9461a5e6d54a256c3a4c74e3fee4f5cba575417
Ashwin Maroli <ashmaroli@gmail.com>
2018-08-02 19:17:07 +0530

    Add `Site#in_cache_dir` helper method

--- test/test_site.rb
+++ test/test_site.rb
@@ -79,7 +79,13 @@ class TestSite < JekyllUnitTest
       site = fixture_site("theme" => "test-theme")
       assert_equal [source_dir("_includes")], site.includes_load_paths
     end
+
+    should "use .jekyll-cache directory at source as cache_dir by default" do
+      site = Site.new default_configuration
+      assert_equal File.join(site.source, ".jekyll-cache"), site.cache_dir
+    end
   end
+
   context "creating sites" do
     setup do
       @site = Site.new(site_configuration)
@@ -664,6 +670,24 @@ class TestSite < JekyllUnitTest
         assert File.file?(dest)
         mtime2 = File.stat(dest).mtime.to_i
         refute_equal mtime1, mtime2 # must be regenerated
+      end
+    end
+
+    context "#in_cache_dir method" do
+      setup do
+        @site = Site.new(
+          site_configuration(
+            "cache_dir" => "../../custom-cache-dir"
+          )
+        )
+      end
+
+      should "create sanitized paths within the cache directory" do
+        assert_equal File.join(@site.source, "custom-cache-dir"), @site.cache_dir
+        assert_equal(
+          File.join(@site.source, "custom-cache-dir", "foo.md.metadata"),
+          @site.in_cache_dir("../../foo.md.metadata")
+        )
       end
     end
   end
