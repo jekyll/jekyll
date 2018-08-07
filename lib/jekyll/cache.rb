5afb0b95f259dd4a86fca6d06747076db31dca00
Pat Hawks <pat@pathawks.com>
2018-08-06 20:35:34 -0500

    Use files on disk to add persistence to cache

--- lib/jekyll/cache.rb
+++ lib/jekyll/cache.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+require "digest"
+require "fileutils"
+require "pstore"
+
 module Jekyll
   class Cache
     extend Forwardable
-
-    def_delegators :@cache, :[], :[]=, :clear, :delete, :key?
 
     # Get an existing named cache, or create a new one if none exists
     #
@@ -14,30 +16,103 @@ module Jekyll
     # rubocop:disable Style/ClassVars
     def initialize(name)
       @@caches ||= {}
-      @@caches[name] ||= {}
-      @cache = @@caches[name]
+      @@caches[name] ||= self
+      @cache = {}
+      @name = name
+      FileUtils.mkdir_p(path_to)
     end
 
     def self.clear
-      @@caches ||= {}
-      @@caches.clear
+      delete_cache_files
+      @@caches.each_value(&:clear)
     end
     # rubocop:enable Style/ClassVars
 
-    # rubocop:disable Lint/UselessSetterCall
-    def self.clear_if_config_changed(config)
-      cache = Jekyll::Cache.new "Jekyll::Cache"
-      clear unless cache["config"] == config
-      cache["config"] = config
+    def clear
+      @cache.clear
     end
-    # rubocop:enable Lint/UselessSetterCall
 
-    def getset(key)
-      if key?(key)
-        @cache[key]
+    # rubocop:disable Security/MarshalLoad
+    def [](key)
+      return @cache[key] if @cache.key?(key)
+      path = cached_hash_path(hash(key))
+      if File.file?(path) && File.readable?(path)
+        cached_file = File.open(path, "rb")
+        value = Marshal.load(cached_file)
+        cached_file.close
+        @cache[key] = value
       else
-        @cache[key] = yield
+        raise
       end
     end
+
+    def getset(key)
+      return @cache[key] if @cache.key?(key)
+      path = cached_hash_path(hash(key))
+      if File.file?(path) && File.readable?(path)
+        cached_file = File.open(path, "rb")
+        value = Marshal.load(cached_file)
+      else
+        value = yield
+        path = cached_hash_path(hash(key))
+        cached_file = File.open(path, "wb")
+        Marshal.dump(value, cached_file)
+      end
+      cached_file.close
+      @cache[key] = value
+    end
+    # rubocop:enable Security/MarshalLoad
+
+    def []=(key, value)
+      @cache[key] = value
+      path = cached_hash_path(hash(key))
+      cached_file = File.open(path, "wb")
+      Marshal.dump(value, cached_file)
+      cached_file.close
+    end
+
+    def delete(key)
+      @cache.delete(key)
+      path = cached_hash_path(hash(key))
+      File.delete(path)
+    end
+
+    def key?(key)
+      return true if @cache.key?(key)
+      path = cached_hash_path(hash(key))
+      File.file?(path) && File.readable?(path)
+    end
+
+    # rubocop:disable Style/ClassVars
+    def self.clear_if_config_changed(config)
+      config = config.inspect
+      cache = Jekyll::Cache.new "Jekyll::Cache"
+      unless cache.key?("config") && cache["config"] == config
+        delete_cache_files
+        @@caches = {}
+        cache = Jekyll::Cache.new "Jekyll::Cache"
+        cache["config"] = config
+      end
+    end
+    # rubocop:enable Style/ClassVars
+
+    private
+
+    def path_to
+      File.expand_path(".jekyll-cache/Jekyll/Cache/#{@name}")
+    end
+
+    def hash(key)
+      Digest::SHA2.hexdigest(key)
+    end
+
+    def cached_hash_path(hash)
+      ".jekyll-cache/Jekyll/Cache/#{@name}/#{hash}"
+    end
+
+    def self.delete_cache_files
+      FileUtils.rm_rf(".jekyll-cache/Jekyll/Cache")
+    end
+    private_class_method :delete_cache_files
   end
 end
