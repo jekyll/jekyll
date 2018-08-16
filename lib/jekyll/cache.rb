0ff29443046e58174f53c96d8689d4faa8ce6219
Pat Hawks <pat@pathawks.com>
2018-08-16 12:41:55 -0500

    Comment all the things

--- lib/jekyll/cache.rb
+++ lib/jekyll/cache.rb
@@ -24,21 +24,28 @@ module Jekyll
       FileUtils.mkdir_p(path_to) if @@disk_cache_enabled
     end
 
+    # Clear all caches
     def self.clear
       delete_cache_files
       @@caches.each_value(&:clear)
     end
 
+    # Disable Marshaling cached items to disk
     def self.disable_disk_cache!
       @@disk_cache_enabled = false
     end
     # rubocop:enable Style/ClassVars
 
+    # Clear this particular cache
     def clear
       delete_cache_files
       @cache.clear
     end
 
+    # Retrieve a cached item
+    # Raises if key does not exist in cache
+    #
+    # Returns cached value
     def [](key)
       return @cache[key] if @cache.key?(key)
       path = path_to(hash(key))
@@ -49,6 +56,9 @@ module Jekyll
       end
     end
 
+    # Add an item to cache
+    #
+    # Returns nothing.
     def []=(key, value)
       @cache[key] = value
       if @@disk_cache_enabled
@@ -57,6 +67,9 @@ module Jekyll
       end
     end
 
+    # If an item already exists in the cache, retrieve it
+    # Else execute code block, and add the result to the cache, and return that
+    #   result
     def getset(key)
       self[key]
     rescue StandardError
@@ -65,6 +78,9 @@ module Jekyll
       value
     end
 
+    # Remove one particular item from the cache
+    #
+    # Returns nothing.
     def delete(key)
       @cache.delete(key)
       if @@disk_cache_enabled
@@ -73,14 +89,23 @@ module Jekyll
       end
     end
 
+    # Check if `key` already exists in this cache
+    #
+    # Returns true if key exists in the cache, false otherwise
     def key?(key)
+      # First, check if item is already cached in memory
       return true if @cache.key?(key)
+      # Otherwise, it might be cached on disk
+      # but we should not consider the disk cache if it is disabled
       return false unless @@disk_cache_enabled
       path = path_to(hash(key))
       File.file?(path) && File.readable?(path)
     end
 
-    # rubocop:disable Style/ClassVars
+    # Compare the current config to the cached config
+    # If they are different, clear all caches
+    #
+    # Returns nothing.
     def self.clear_if_config_changed(config)
       config = config.inspect
       cache = Jekyll::Cache.new "Jekyll::Cache"
@@ -90,24 +115,32 @@ module Jekyll
         cache["config"] = config
       end
     end
-    # rubocop:enable Style/ClassVars
 
     private
 
+    # Given a hashed key, return the path to where this item would be saved on
+    # disk
     def path_to(hash = nil)
       @base_dir ||= File.join(@@base_dir, @name)
       return @base_dir if hash.nil?
       File.join(@base_dir, hash[0..1], hash[2..-1]).freeze
     end
 
+    # Given a key, return a SHA2 hash that can be used for caching this item to
+    # disk
     def hash(key)
       Digest::SHA2.hexdigest(key).freeze
     end
 
+    # Remove all this caches items from disk
+    #
+    # Returns nothing.
     def delete_cache_files
       FileUtils.rm_rf(path_to) if @@disk_cache_enabled
     end
 
+    # Load `path` from disk and return the result
+    # This MUST NEVER be called in Safe Mode
     # rubocop:disable Security/MarshalLoad
     def load(path)
       raise unless @@disk_cache_enabled
@@ -118,6 +151,10 @@ module Jekyll
     end
     # rubocop:enable Security/MarshalLoad
 
+    # Given a path and a value, save value to disk at path
+    # This should NEVER be called in Safe Mode
+    #
+    # Returns nothing.
     def dump(path, value)
       return unless @@disk_cache_enabled
       dir = File.dirname(path)
@@ -127,6 +164,9 @@ module Jekyll
       end
     end
 
+    # Delete all cached items from all caches
+    #
+    # Returns nothing.
     def self.delete_cache_files
       FileUtils.rm_rf(@@base_dir) if @@disk_cache_enabled
     end
