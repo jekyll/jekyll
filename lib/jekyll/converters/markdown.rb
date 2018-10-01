55dd39d580250299347d841376a31e1b0bbbb49f
Grzegorz Kaczorek <grzegorz.kaczorek@gmail.com>
2018-10-01 20:37:15 +0200

    Document converter methods (#7289)
    Merge pull request 7289

--- lib/jekyll/converters/markdown.rb
+++ lib/jekyll/converters/markdown.rb
@@ -2,6 +2,8 @@
 
 module Jekyll
   module Converters
+    # Markdown converter.
+    # For more info on converters see https://jekyllrb.com/docs/plugins/converters/
     class Markdown < Converter
       highlighter_prefix "\n"
       highlighter_suffix "\n"
@@ -61,14 +63,30 @@ module Jekyll
         end
       end
 
+      # Does the given extension match this converter's list of acceptable extensions?
+      # Takes one argument: the file's extension (including the dot).
+      #
+      # ext - The String extension to check.
+      #
+      # Returns true if it matches, false otherwise.
       def matches(ext)
         extname_list.include?(ext.downcase)
       end
 
+      # Public: The extension to be given to the output file (including the dot).
+      #
+      # ext - The String extension or original file.
+      #
+      # Returns The String output file extension.
       def output_ext(_ext)
         ".html"
       end
 
+      # Logic to do the content conversion.
+      #
+      # content - String content of file (without front matter).
+      #
+      # Returns a String of the converted content.
       def convert(content)
         setup
         @parser.convert(content)
