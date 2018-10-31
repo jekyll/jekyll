f0af098abe6b5881f1ea361615e58a6fa3dbb987
Ashwin Maroli <ashmaroli@users.noreply.github.com>
2018-10-31 22:40:19 +0530

    Memoize the return value of Site#documents (#7273)
    Merge pull request 7273

--- lib/jekyll/site.rb
+++ lib/jekyll/site.rb
@@ -97,6 +97,7 @@ module Jekyll
       @post_attr_hash = {}
       @site_data = nil
       @collections = nil
+      @documents = nil
       @docs_to_write = nil
       @regenerator.clear_cache
       @liquid_renderer.reset
@@ -326,7 +327,7 @@ module Jekyll
     #
     # Returns an Array of all Documents
     def documents
-      collections.reduce(Set.new) do |docs, (_, collection)|
+      @documents ||= collections.reduce(Set.new) do |docs, (_, collection)|
         docs + collection.docs + collection.files
       end.to_a
     end
