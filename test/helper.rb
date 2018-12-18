16fb1eab67fc487646345942111bb381187b701b
Ashwin Maroli <ashmaroli@gmail.com>
2018-12-18 13:27:30 +0530

    Create symlink only if target is accessible

--- test/helper.rb
+++ test/helper.rb
@@ -190,6 +190,13 @@ class JekyllUnitTest < Minitest::Test
       skip msg.to_s.magenta
     end
   end
+
+  def symlink_if_allowed(target, sym_file)
+    FileUtils.ln_sf(target, sym_file)
+  rescue Errno::EACCES
+    skip "Permission denied for creating a symlink to #{target.inspect} " \
+         "on this machine".magenta
+  end
 end
 
 class FakeLogger
