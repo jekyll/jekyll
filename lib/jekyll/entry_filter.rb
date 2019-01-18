d94c91220962864815f97d781846766e0e108738
Ashwin Maroli <ashmaroli@gmail.com>
2019-01-18 21:37:38 +0530

    Filter out exclusively excluded entries sooner

--- lib/jekyll/entry_filter.rb
+++ lib/jekyll/entry_filter.rb
@@ -31,13 +31,20 @@ module Jekyll
 
     def filter(entries)
       entries.reject do |e|
-        # Reject this entry if it is a symlink.
-        next true if symlink?(e)
-        # Do not reject this entry if it is included.
-        next false if included?(e)
+        # Check if the current entry is explicitly included and cache the result
+        included = included?(e)
 
-        # Reject this entry if it is special, a backup file, or excluded.
-        special?(e) || backup?(e) || excluded?(e)
+        # Reject current entry if it is excluded but not explicitly included as well.
+        next true if excluded?(e) && !included
+
+        # Reject current entry if it is a symlink.
+        next true if symlink?(e)
+
+        # Do not reject current entry if it is explicitly included.
+        next false if included
+
+        # Reject current entry if it is special or a backup file.
+        special?(e) || backup?(e)
       end
     end
 
