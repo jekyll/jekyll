68610e8115247a1557fb0129e8d8ce264bf26243
Ashwin Maroli <ashmaroli@gmail.com>
2019-01-30 10:06:07 +0530

    Add some clarity to the proceedings

--- lib/jekyll/filters.rb
+++ lib/jekyll/filters.rb
@@ -365,9 +365,10 @@ module Jekyll
       end
     end
 
+    # -----------   The following set of code was *adapted* from Liquid::If
+    # -----------   ref: https://git.io/vp6K6
+
     # Parse a string to a Liquid Condition
-    # The following code was *adapted* from Liquid::If
-    # ref: https://git.io/vp6K6
     def parse_condition(exp)
       parser = Liquid::Parser.new(exp)
       condition = parse_binary_comparison(parser)
@@ -375,24 +376,37 @@ module Jekyll
       condition
     end
 
+    # Generate a Liquid::Condition object from a Liquid::Parser object additionally processing
+    # the parsed expression based on whether the expression consists of binary operations with
+    # Liquid operators `and` or `or`
+    #
+    #  - parser: an instance of Liquid::Parser
+    #
+    # Returns an instance of Liquid::Condition
     def parse_binary_comparison(parser)
-      condition = parse_comparison(parser)
-      binary_operator = parser.id?("and") || parser.id?("or")
-      condition.send(binary_operator, parse_binary_comparison(parser)) if binary_operator
-      condition
+      parse_comparison(parser).tap do |condition|
+        binary_operator = parser.id?("and") || parser.id?("or")
+        condition.send(binary_operator, parse_comparison(parser)) if binary_operator
+      end
     end
 
+    # Generates a Liquid::Condition object from a Liquid::Parser object based on whether the parsed
+    # expression involves a "comparison" operator (e.g. <, ==, >, !=, etc)
+    #
+    #  - parser: an instance of Liquid::Parser
+    #
+    # Returns an instance of Liquid::Condition
     def parse_comparison(parser)
-      parsed_left_expr = Liquid::Expression.parse(parser.expression)
+      left_operand = Liquid::Expression.parse(parser.expression)
       operator = parser.consume?(:comparison)
       if operator
-        Liquid::Condition.new(
-          parsed_left_expr,
-          operator,
-          Liquid::Expression.parse(parser.expression)
-        )
+        # 1. Parse what remains after "consuming" the left operand and the `:comparison` operator
+        # 2. Initialize a Liquid::Condition using the operands and the comparison-operator
+        right_operand = Liquid::Expression.parse(parser.expression)
+        Liquid::Condition.new(left_operand, operator, right_operand)
       else
-        Liquid::Condition.new(parsed_left_expr)
+        # No comparison-operator detected. Initialize a Liquid::Condition using only left operand
+        Liquid::Condition.new(left_operand)
       end
     end
   end
