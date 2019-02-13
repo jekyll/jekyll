1d68b1716c601f3b0d9d12906eda0f7ec8a512af
Ashwin Maroli <ashmaroli@gmail.com>
2019-02-13 20:31:49 +0530

    Remove memoizations introduced

--- lib/jekyll/utils/platforms.rb
+++ lib/jekyll/utils/platforms.rb
@@ -14,30 +14,30 @@ module Jekyll
       end
 
       def windows?
-        @windows ||= vanilla_windows? || bash_on_windows?
+        vanilla_windows? || bash_on_windows?
       end
 
       # Not a Windows Subsystem for Linux (WSL)
       def vanilla_windows?
-        @vanilla_windows ||= !!(rbconfig_host =~ %r!mswin|mingw|cygwin!) && proc_version.empty?
+        !!(rbconfig_host =~ %r!mswin|mingw|cygwin!) && proc_version.empty?
       end
       alias_method :really_windows?, :vanilla_windows?
 
       # Determine if Windows Subsystem for Linux (WSL)
       def bash_on_windows?
-        @bash_on_windows ||= linux_os? && microsoft_proc_version?
+        linux_os? && microsoft_proc_version?
       end
 
       def linux?
-        @linux ||= linux_os? && !microsoft_proc_version?
+        linux_os? && !microsoft_proc_version?
       end
 
       def osx?
-        @osx ||= !!(rbconfig_host =~ %r!darwin|mac os!)
+        !!(rbconfig_host =~ %r!darwin|mac os!)
       end
 
       def unix?
-        @unix ||= !!(rbconfig_host =~ %r!solaris|bsd!)
+        !!(rbconfig_host =~ %r!solaris|bsd!)
       end
 
       private
