da313bf883d2f285ae444066e55c2520c5c3bb93
Ashwin Maroli <ashmaroli@users.noreply.github.com>
2019-03-15 22:41:46 +0530

    Avoid unnecessary duplication of pages array (#7272)
    Merge pull request 7272

--- lib/jekyll/site.rb
+++ lib/jekyll/site.rb
@@ -512,7 +512,7 @@ module Jekyll
     end
 
     def render_pages(payload)
-      pages.flatten.each do |page|
+      pages.each do |page|
         render_regenerated(page, payload)
       end
     end
