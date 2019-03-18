647c5931bc389d3beec4760723894e58a4d943e3
Ashwin Maroli <ashmaroli@gmail.com>
2019-03-18 11:31:15 +0530

    Backport 3c06609 to 3.7-stable
    Install platform-specific gems as required

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -87,10 +87,14 @@ group :jekyll_plugins do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+# and associated library.
+install_if -> { RUBY_PLATFORM =~ %r!mingw|mswin|java! } do
+  gem "tzinfo", "~> 1.2"
+  gem "tzinfo-data"
+end
 
 # Performance-booster for watching directories on Windows
-gem "wdm", "~> 0.1.0" if Gem.win_platform?
+gem "wdm", "~> 0.1.0", :install_if => Gem.win_platform?
 
 RUBY
         end
