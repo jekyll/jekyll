97a5b4e1171c5b2092987658aafc25592128937a
Ashwin Maroli <ashmaroli@gmail.com>
2019-04-19 12:59:23 +0530

    prefer String#start_with? over anchored regex

--- lib/jekyll/commands/serve/websockets.rb
+++ lib/jekyll/commands/serve/websockets.rb
@@ -46,7 +46,7 @@ module Jekyll
           # WebSockets requests will have a Connection: Upgrade header
           if parser.http_method != "GET" || parser.upgrade?
             super
-          elsif %r!^/livereload.js!.match?(parser.request_url)
+          elsif parser.request_url.start_with?("/livereload.js")
             headers = [
               "HTTP/1.1 200 OK",
               "Content-Type: application/javascript",
