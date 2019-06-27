2d93e96d8dd9459be360d26e7e093b5e44332dc1
Ashwin Maroli <ashmaroli@gmail.com>
2019-06-27 19:40:49 +0530

    Cache interim paths in EntryFilter#glob_include?

--- lib/jekyll/entry_filter.rb
+++ lib/jekyll/entry_filter.rb
@@ -5,6 +5,12 @@ module Jekyll
     attr_reader :site
     SPECIAL_LEADING_CHAR_REGEX = %r!\A#{Regexp.union([".", "_", "#", "~"])}!o.freeze
 
+    def self.prefix_source(src, item)
+      @prefix_source ||= {}
+      @prefix_source[src] ||= {}
+      @prefix_source[src][item] ||= File.join(src, item)
+    end
+
     def initialize(site, base_directory = nil)
       @site = site
       @base_directory = derive_base_directory(
@@ -90,12 +96,12 @@ module Jekyll
     # Check if an entry matches a specific pattern.
     # Returns true if path matches against any glob pattern, else false.
     def glob_include?(enumerator, entry)
-      entry_with_source = File.join(site.source, entry)
+      entry_with_source = EntryFilter.prefix_source(site.source, entry)
 
       enumerator.any? do |pattern|
         case pattern
         when String
-          pattern_with_source = File.join(site.source, pattern)
+          pattern_with_source = EntryFilter.prefix_source(site.source, pattern)
 
           File.fnmatch?(pattern_with_source, entry_with_source) ||
             entry_with_source.start_with?(pattern_with_source)
