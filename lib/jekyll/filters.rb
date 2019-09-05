1d372034178dbf5a4f80a5db680c8a6082078c98
iBug <git@ibugone.com>
2019-09-05 21:13:44 +0800

    RuboCop is hard to satisfy...

--- lib/jekyll/filters.rb
+++ lib/jekyll/filters.rb
@@ -122,8 +122,8 @@ module Jekyll
     #
     # Returns the Integer word count.
     def number_of_words(input)
-      cjk_regex = /\p{Han}|\p{Katakana}|\p{Hiragana}|\p{Hangul}/
-      input.scan(cjk_regex).length + input.gsub(cjk_regex, ' ').split.length
+      cjk_regex = %r(\p{Han}|\p{Katakana}|\p{Hiragana}|\p{Hangul})
+      input.scan(cjk_regex).length + input.gsub(cjk_regex, " ").split.length
     end
 
     # Join an array of things into a string by separating with commas and the
