83869edcc495cae1aba5b233d014ac00dfc10aa4
Ashwin Maroli <ashmaroli@gmail.com>
2019-12-26 22:48:23 +0530

    Allow extensionless document in a strict site

--- lib/jekyll/document.rb
+++ lib/jekyll/document.rb
@@ -497,6 +497,10 @@ module Jekyll
       elsif relative_path =~ DATELESS_FILENAME_MATCHER
         slug, ext = Regexp.last_match.captures
       end
+      # `slug` will be nil for documents without an extension since the regex patterns
+      # above tests for an extension as well.
+      # In such cases, assign `basename_without_ext` as the slug.
+      slug ||= basename_without_ext
 
       # slugs shouldn't end with a period
       # `String#gsub!` removes all trailing periods (in comparison to `String#chomp!`)
