b6f077f5175468f1a07b991a1469e975d553bc0a
Ashwin Maroli <ashmaroli@gmail.com>
2020-01-17 21:21:36 +0530

    Integrate Profiler into LiquidRenderer::Table

--- lib/jekyll/profiler.rb
+++ lib/jekyll/profiler.rb
@@ -2,59 +2,57 @@
 
 module Jekyll
   class Profiler
+    TERMINAL_TABLE_STYLES = {
+      :alignment     => :right,
+      :border_top    => false,
+      :border_bottom => false,
+    }.freeze
+    private_constant :TERMINAL_TABLE_STYLES
+
+    def self.tabulate(table_rows)
+      require "terminal-table"
+
+      rows   = table_rows.dup
+      header = rows.shift
+      footer = rows.pop
+      output = +"\n"
+
+      table = Terminal::Table.new do |t|
+        t << header
+        t << :separator
+        rows.each { |row| t << row }
+        t << :separator
+        t << footer
+        t.style = TERMINAL_TABLE_STYLES
+        t.align_column(0, :left)
+      end
+
+      output << table.to_s << "\n"
+    end
+
     def initialize(site)
       @site = site
     end
 
     def profile_process
-      profile_data = {}
+      profile_data = { "PHASE" => "TIME" }
       total_time = 0
 
       [:reset, :read, :generate, :render, :cleanup, :write].each do |method|
         start_time = Time.now
         @site.send(method)
         end_time = (Time.now - start_time).round(4)
-        profile_data[method.to_s.upcase] = end_time
+        profile_data[method.to_s.upcase.cyan] = format("%.4f", end_time)
         total_time += end_time
       end
 
-      profile_data["TOTAL TIME"] = total_time
-      Jekyll.logger.info print_phase_stats(profile_data)
+      profile_data["TOTAL TIME"] = format("%.4f", total_time)
 
-      Jekyll.logger.info "\nSite Render Stats:\n------------------"
+      Jekyll.logger.info "\nBuild Process Summary:".cyan
+      Jekyll.logger.info Profiler.tabulate(Array(profile_data))
+
+      Jekyll.logger.info "\nSite Render Stats:".cyan
       @site.print_stats
-    end
-
-    private
-
-    # rubocop:disable Metrics/AbcSize
-    def print_phase_stats(data)
-      cell_width  = data.keys.map(&:length).max
-      cell_border = "-" * (cell_width + 2)
-      total_time  = data.delete("TOTAL TIME")
-
-      table = +"\nBuild Process Summary:\n"
-      table << "-" * 22 << "\n\n"
-      table << format_data_cell("PHASE", "TIME", cell_width)
-      table << divider(cell_border)
-
-      data.each do |key, value|
-        table << format_data_cell(key, format("%.4f", value), cell_width)
-      end
-
-      table << divider(cell_border)
-      table << format_data_cell("TOTAL TIME", format("%.4f", total_time), cell_width)
-    end
-    # rubocop:enable Metrics/AbcSize
-
-    def format_data_cell(key, val, width)
-      row = +""
-      row << key.to_s.ljust(width - 2).center(width + 2) << " | "
-      row << val.to_s.rjust(width) << "\n"
-    end
-
-    def divider(cell_border)
-      +"" << cell_border << "-+-" << cell_border << "\n"
     end
   end
 end
