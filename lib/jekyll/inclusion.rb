5b81aa945351b99c5f034e1c3b5eaa7e775d41db
Ashwin Maroli <ashmaroli@gmail.com>
2020-05-14 15:12:19 +0530

    Make `Inclusion#relative_path` a private method

--- lib/jekyll/inclusion.rb
+++ lib/jekyll/inclusion.rb
@@ -10,10 +10,6 @@ module Jekyll
       @path = PathManager.join(base, name)
     end
 
-    def relative_path
-      @relative_path ||= compute_relative_path
-    end
-
     def render(context)
       @template ||= site.liquid_renderer.file(relative_path).parse(content)
       @template.render!(context)
@@ -36,23 +32,18 @@ module Jekyll
 
     attr_reader :site
 
-    def dir_path
-      if site.theme && path.start_with?(site.theme.includes_path)
-        PathManager.join site.theme.basename, "_includes"
-      else
-        site.config["includes_dir"]
-      end
-    end
-
     # rubocop:disable Metrics/AbcSize
-    def compute_relative_path
-      case path
-      when site.theme && %r!#{site.theme.includes_path}/(?<rel_path>.*)!o
-        PathManager.join dir_path, Regexp.last_match[:rel_path]
-      when %r!#{site.in_source_dir(site.config["includes_dir"], "/")}(?<rel_path>.*)!o
-        PathManager.join dir_path, Regexp.last_match[:rel_path]
-      else
-        path.sub(site.in_source_dir("/"), "")
+    def relative_path
+      @relative_path ||= begin
+        case path
+        when site.theme && %r!#{site.theme.includes_path}/(?<rel_path>.*)!o
+          dir_path = PathManager.join(site.theme.basename, "_includes")
+          PathManager.join(dir_path, Regexp.last_match[:rel_path])
+        when %r!#{site.in_source_dir(site.config["includes_dir"], "/")}(?<rel_path>.*)!o
+          PathManager.join(site.config["includes_dir"], Regexp.last_match[:rel_path])
+        else
+          path.sub(site.in_source_dir("/"), "")
+        end
       end
     end
     # rubocop:enable Metrics/AbcSize
