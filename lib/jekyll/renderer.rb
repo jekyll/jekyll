d728bcc3275fc0ec1f54c7bcf9b2d22ff720b11c
Liam Cooke <liam@liamcooke.com>
2020-09-03 21:18:47 +1000

    Remove extra hook argument

--- lib/jekyll/renderer.rb
+++ lib/jekyll/renderer.rb
@@ -85,7 +85,7 @@ module Jekyll
       document.content = output
 
       # Hook to modify converted content before layout is applied
-      document.trigger_hooks(:post_convert, output)
+      document.trigger_hooks(:post_convert)
       output = document.content
 
       if document.place_in_layout?
