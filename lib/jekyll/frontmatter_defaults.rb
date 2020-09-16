2ad318350a203eb2addef4b5df2e577b3a6da77d
Ashwin Maroli <ashmaroli@gmail.com>
2020-09-16 22:04:35 +0530

    Check default front matter scope against symbols

--- lib/jekyll/frontmatter_defaults.rb
+++ lib/jekyll/frontmatter_defaults.rb
@@ -157,7 +157,7 @@ module Jekyll
     # Returns true if either of the above conditions are satisfied,
     #   otherwise returns false
     def applies_type?(scope, type)
-      !scope.key?("type") || scope["type"].eql?(type.to_s)
+      !scope.key?("type") || type&.to_sym.eql?(scope["type"].to_sym)
     end
 
     # Checks if a given set of default values is valid
