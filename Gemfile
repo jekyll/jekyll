341401baf5a0287e4cb3a0770ac3bca4d79df883
Ashwin Maroli <ashmaroli@gmail.com>
2020-10-21 19:00:39 +0530

    Point Gemfile to the Liquid repository

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@
 source "https://rubygems.org"
 gemspec :name => "jekyll"
 
+gem "liquid", :github => "Shopify/liquid"
 gem "rake", "~> 13.0"
 
 group :development do
--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -79,6 +79,9 @@ module Jekyll
             # This will help ensure the proper Jekyll version is running.
             # Happy Jekylling!
             gem "jekyll", "~> #{Jekyll::VERSION}"
+
+            gem "liquid", :github => "Shopify/liquid"
+
             # This is the default theme for new Jekyll sites. You may change this to anything you like.
             gem "minima", "~> 2.5"
             # If you want to use GitHub Pages, remove the "gem "jekyll"" above and
