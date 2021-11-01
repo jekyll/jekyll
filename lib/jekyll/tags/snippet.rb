46cd5fd3bedda56a02f00d37901dda61bfde3346
Ashwin Maroli <ashmaroli@gmail.com>
2021-11-01 12:34:18 +0530

    Allow registering snippet as regen dependency

--- lib/jekyll/tags/snippet.rb
+++ lib/jekyll/tags/snippet.rb
@@ -14,10 +14,29 @@ module Jekyll
         relative_path = @relative_path&.render(context)
         snippet = @site.snippets[relative_path]
 
-        return (snippet.output || "") if snippet
+        if snippet
+          add_as_dependency(snippet, context) if @site.config["incremental"]
+          return snippet.output
+        end
 
         Jekyll.logger.warn "Snippet: #{relative_path.inspect} not found."
         ""
+      end
+
+      private
+
+      def add_as_dependency(snippet, context)
+        page = context.registers[:page]
+        return unless page&.key?("path")
+
+        absolute_path = \
+          if page["collection"]
+            @site.in_source_dir(@site.config["collections_dir"], page["path"])
+          else
+            @site.in_source_dir(page["path"])
+          end
+
+        @site.regenerator.add_dependency(absolute_path, snippet.path)
       end
     end
 
