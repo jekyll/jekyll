b6ba77caa1f906384e5afedcde3ca09dc5498f12
Ashwin Maroli <ashmaroli@gmail.com>
2021-11-14 20:04:12 +0530

    Delegate to instead of inheriting Ruby core class

--- lib/jekyll/site_data/directory.rb
+++ lib/jekyll/site_data/directory.rb
@@ -2,13 +2,33 @@
 
 module Jekyll
   module SiteData
-    class Directory < Hash
+    class Directory
       attr_accessor :context
 
+      def initialize
+        @meta = {}
+      end
+
       def [](key)
-        super.tap do |value|
+        @meta[key].tap do |value|
           value.context = context if value.respond_to?(:context=)
         end
+      end
+
+      def to_liquid
+        self
+      end
+
+      def method_missing(method, *args, &block)
+        @meta.send(method, *args, &block)
+      end
+
+      def respond_to_missing?(method, *)
+        @meta.respond_to?(method)
+      end
+
+      def inspect
+        @meta.inspect
       end
     end
   end
