4a06db8810f0faaca0267345157e956f345bb914
Phil Ross <philr@users.noreply.github.com>
2021-12-08 11:35:42 +0000

    Backport support_tzinfo_v2 from #8880 to 3.9-stable [merge conflicts]

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -65,6 +65,7 @@ module Jekyll
           <<-RUBY
 source "https://rubygems.org"
 
+<<<<<<< HEAD
 # Hello! This is where you manage which Jekyll version is used to run.
 # When you want to use a different version, change it below, save the
 # file and run `bundle install`. Run Jekyll with `bundle exec`, like so:
@@ -74,6 +75,14 @@ source "https://rubygems.org"
 # This will help ensure the proper Jekyll version is running.
 # Happy Jekylling!
 gem "jekyll", "~> #{Jekyll::VERSION}"
+=======
+            # Windows and JRuby does not include zoneinfo files, so bundle the tzinfo-data gem
+            # and associated library.
+            platforms :mingw, :x64_mingw, :mswin, :jruby do
+              gem "tzinfo", ">= 1", "< 3"
+              gem "tzinfo-data"
+            end
+>>>>>>> 9c9cf3e82 (Support both tzinfo v1 and v2 alongwith non-half hour offsets. (#8880))
 
 # This is the default theme for new Jekyll sites. You may change this to anything you like.
 gem "minima", "~> 2.0"
