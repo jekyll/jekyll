95fb1c91c17bdace0dbc2cc6cb1fe3022bd5292e
Ashwin Maroli <ashmaroli@gmail.com>
2022-11-06 19:47:28 +0530

    Use percent literals to avoid escaping double-quotes

--- lib/jekyll/tags/highlight.rb
+++ lib/jekyll/tags/highlight.rb
@@ -120,10 +120,10 @@ module Jekyll
 
       def add_code_tag(code)
         code_attributes = [
-          "class=\"language-#{@lang.to_s.tr("+", "-")}\"",
-          "data-lang=\"#{@lang}\"",
+          %(class="language-#{@lang.to_s.tr("+", "-")}"),
+          %(data-lang="#{@lang}"),
         ].join(" ")
-        "<figure class=\"highlight\"><pre><code #{code_attributes}>" \
+        %(<figure class="highlight"><pre><code #{code_attributes}>) \
           "#{code.chomp}</code></pre></figure>"
       end
     end
