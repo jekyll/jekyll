c86f22e6f5e08083dadde1262f65444c1edba136
Ashwin Maroli <ashmaroli@gmail.com>
2022-11-07 11:00:25 +0530

    Optimize by making use of allowed line-length

--- lib/jekyll/tags/highlight.rb
+++ lib/jekyll/tags/highlight.rb
@@ -119,12 +119,8 @@ module Jekyll
       end
 
       def add_code_tag(code)
-        code_attributes = [
-          %(class="language-#{@lang.to_s.tr("+", "-")}"),
-          %(data-lang="#{@lang}"),
-        ].join(" ")
-        %(<figure class="highlight"><pre><code #{code_attributes}>) \
-          "#{code.chomp}</code></pre></figure>"
+        code_attrs = %(class="language-#{@lang.to_s.tr("+", "-")}" data-lang="#{@lang}")
+        %(<figure class="highlight"><pre><code #{code_attrs}>#{code.chomp}</code></pre></figure>)
       end
     end
   end
