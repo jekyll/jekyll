17ce428dc4dfe98e2666b80214badc39f681fec3
Parker Moore <parkrmoore@gmail.com>
2023-01-25 14:54:24 -0800

    v3.9.x: fix test for the latest version of rdiscount

--- test/test_rdiscount.rb
+++ test/test_rdiscount.rb
@@ -28,16 +28,16 @@ class TestRdiscount < JekyllUnitTest
 
     should "render toc" do
       toc = <<-TOC
-<a name="Header.1"></a>
+<a name="Header-1"></a>
 <h1>Header 1</h1>
 
-<a name="Header.2"></a>
+<a name="Header-2"></a>
 <h2>Header 2</h2>
 
 <p><ul>
- <li><a href="#Header.1">Header 1</a>
+ <li><a href="#Header-1">Header 1</a>
  <ul>
-  <li><a href="#Header.2">Header 2</a></li>
+  <li><a href="#Header-2">Header 2</a></li>
  </ul>
  </li>
 </ul>
