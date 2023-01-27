1fbb8f30c3d88eba7e97556b40060e3cf898dc09
Parker Moore <parkrmoore@gmail.com>
2023-01-26 21:45:09 -0800

    Fix merge conflicts merging PR 8880

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -65,7 +65,6 @@ module Jekyll
           <<-RUBY
 source "https://rubygems.org"
 
-<<<<<<< HEAD
 # Hello! This is where you manage which Jekyll version is used to run.
 # When you want to use a different version, change it below, save the
 # file and run `bundle install`. Run Jekyll with `bundle exec`, like so:
@@ -75,14 +74,6 @@ source "https://rubygems.org"
 # This will help ensure the proper Jekyll version is running.
 # Happy Jekylling!
 gem "jekyll", "~> #{Jekyll::VERSION}"
-=======
-            # Windows and JRuby does not include zoneinfo files, so bundle the tzinfo-data gem
-            # and associated library.
-            platforms :mingw, :x64_mingw, :mswin, :jruby do
-              gem "tzinfo", ">= 1", "< 3"
-              gem "tzinfo-data"
-            end
->>>>>>> 9c9cf3e82 (Support both tzinfo v1 and v2 alongwith non-half hour offsets. (#8880))
 
 # This is the default theme for new Jekyll sites. You may change this to anything you like.
 gem "minima", "~> 2.0"
@@ -96,10 +87,10 @@ group :jekyll_plugins do
   gem "jekyll-feed", "~> 0.6"
 end
 
-# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+# Windows and JRuby does not include zoneinfo files, so bundle the tzinfo-data gem
 # and associated library.
-install_if -> { RUBY_PLATFORM =~ %r!mingw|mswin|java! } do
-  gem "tzinfo", "~> 1.2"
+platforms :mingw, :x64_mingw, :mswin, :jruby do
+  gem "tzinfo", ">= 1", "< 3"
   gem "tzinfo-data"
 end
 
--- lib/jekyll/utils/win_tz.rb
+++ lib/jekyll/utils/win_tz.rb
@@ -11,22 +11,16 @@ module Jekyll
       # timezone - the IANA Time Zone specified in "_config.yml"
       #
       # Returns a string that ultimately re-defines ENV["TZ"] in Windows
-      def calculate(timezone, now = Time.now)
+      def calculate(timezone)
         External.require_with_graceful_fail("tzinfo") unless defined?(TZInfo)
         tz = TZInfo::Timezone.get(timezone)
-
-        #
-        # Use period_for_utc and utc_total_offset instead of
-        # period_for and observed_utc_offset for compatibility with tzinfo v1.
-        offset = tz.period_for_utc(now.getutc).utc_total_offset
-
+        difference = Time.now.to_i - tz.now.to_i
         #
         # POSIX style definition reverses the offset sign.
         #   e.g. Eastern Standard Time (EST) that is 5Hrs. to the 'west' of Prime Meridian
         #   is denoted as:
         #     EST+5 (or) EST+05:00
         # Reference: http://www.gnu.org/software/libc/manual/html_node/TZ-Variable.html
-<<<<<<< HEAD
         sign = difference < 0 ? "-" : "+"
         offset = sign == "-" ? "+" : "-" unless difference.zero?
         #
@@ -38,22 +32,43 @@ module Jekyll
         # Establish the minutes based on modulo expression.
         hh = format("%02d", absolute_hour(difference).ceil)
         mm = modulo.zero? ? "00" : "30"
-=======
-        sign = offset.positive? ? "-" : "+"
->>>>>>> 9c9cf3e82 (Support both tzinfo v1 and v2 alongwith non-half hour offsets. (#8880))
 
-        rational_hours = offset.abs.to_r / 3600
-        hours = rational_hours.to_i
-        minutes = ((rational_hours - hours) * 60).to_i
-
-        #
-        # Format the hours and minutes as two-digit numbers.
-        time = format("%<hours>02d:%<minutes>02d", :hours => hours, :minutes => minutes)
-
-        Jekyll.logger.debug "Timezone:", "#{timezone} #{sign}#{time}"
+        Jekyll.logger.debug "Timezone:", "#{timezone} #{offset}#{hh}:#{mm}"
         #
         # Note: The 3-letter-word below doesn't have a particular significance.
-        "WTZ#{sign}#{time}"
+        "WTZ#{sign}#{hh}:#{mm}"
+      end
+
+      private
+
+      # Private: Convert given seconds to an hour as a rational number.
+      #
+      # seconds - supplied as an integer, it is converted to a rational number.
+      # 3600 - no. of seconds in an hour.
+      #
+      # Returns a rational number.
+      def rational_hour(seconds)
+        seconds.to_r / 3600
+      end
+
+      # Private: Convert given seconds to an hour as an absolute number.
+      #
+      # seconds - supplied as an integer, it is converted to its absolute.
+      # 3600 - no. of seconds in an hour.
+      #
+      # Returns an integer.
+      def absolute_hour(seconds)
+        seconds.abs / 3600
+      end
+
+      # Private: Perform a modulo operation on a given fraction.
+      #
+      # fraction - supplied as a rational number, its numerator is divided
+      #            by its denominator and the remainder returned.
+      #
+      # Returns an integer.
+      def modulo_of(fraction)
+        fraction.numerator % fraction.denominator
       end
     end
   end
