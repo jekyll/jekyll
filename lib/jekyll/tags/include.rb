08369c20ebe779bc63d2412b4bd8af50cd32028e
Kip Landergren <klandergren@users.noreply.github.com>
2023-02-13 14:27:08 -0800

    first pass of IncludeRawTag
    just overrides `read_file` to wrap the content in raw liquid tags. does not
    attempt to mess with the OptimizedIncludeTag approach or worry about
    registering a file path with the liquid renderer that likely shouldn't be
    rendered directly.

--- lib/jekyll/tags/include.rb
+++ lib/jekyll/tags/include.rb
@@ -268,8 +268,17 @@ module Jekyll
         path.delete_suffix("/#excerpt")
       end
     end
+
+    class IncludeRawTag < IncludeTag
+      def read_file(file, context)
+        contents = File.read(file, **file_read_opts(context))
+        "{% raw %}#{contents}{% endraw %}"
+      end
+    end
+
   end
 end
 
 Liquid::Template.register_tag("include", Jekyll::Tags::OptimizedIncludeTag)
 Liquid::Template.register_tag("include_relative", Jekyll::Tags::IncludeRelativeTag)
+Liquid::Template.register_tag("include_raw", Jekyll::Tags::IncludeRawTag)
