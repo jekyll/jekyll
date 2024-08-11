640427bae9777f924106a1730603ff3238891505
Aerom Xundes <Aerom.Xundes@gmail.com>
2024-08-10 22:37:02 -0400

    Remove broken wdm dependency of new Jekyll site template. Fixes #9615

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -96,9 +96,6 @@ module Jekyll
               gem "tzinfo-data"
             end
 
-            # Performance-booster for watching directories on Windows
-            gem "wdm", "~> 0.1.1", :platforms => [:mingw, :x64_mingw, :mswin]
-
             # Lock `http_parser.rb` gem to `v0.6.x` on JRuby builds since newer versions of the gem
             # do not have a Java counterpart.
             gem "http_parser.rb", "~> 0.6.0", :platforms => [:jruby]
