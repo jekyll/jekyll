02d3bf2353a39e380bc5952ec12a2c78102c65a0
Ashwin Maroli <ashmaroli@users.noreply.github.com>
2024-09-16 21:03:45 +0530

    Backport #9662 for v4.3.x: Relax version constraint on `wdm` in new Gemfile (#9683)
    This backports 3a8b282 to 4.3-stable

--- lib/jekyll/commands/new.rb
+++ lib/jekyll/commands/new.rb
@@ -97,7 +97,7 @@ module Jekyll
             end
 
             # Performance-booster for watching directories on Windows
-            gem "wdm", "~> 0.1.1", :platforms => [:mingw, :x64_mingw, :mswin]
+            gem "wdm", "~> 0.1", :platforms => [:mingw, :x64_mingw, :mswin]
 
             # Lock `http_parser.rb` gem to `v0.6.x` on JRuby builds since newer versions of the gem
             # do not have a Java counterpart.
