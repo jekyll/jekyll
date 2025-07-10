55422e6093114b204df5db201c7e0b6106d4df12
f <f@sutty.nl>
2025-07-10 11:34:27 -0300

    fix: test unescaping colons in urls

--- test/test_url.rb
+++ test/test_url.rb
@@ -87,5 +87,9 @@ class TestURL < JekyllUnitTest
         :placeholders => { :x => "foo", :y => "foo bar:foobar" }
       ).to_s
     end
+
+    should "unescape urls with colons" do
+      assert_equal "/foo/foo bar:foobar/", Jekyll::URL.unescape_path("/foo/foo%20bar:foobar/")
+    end
   end
 end
