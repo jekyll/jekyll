8519b26a8e3dfa5e42d194b44f8e1f2731408393
Mike Dalton <michaelcdalton@gmail.com>
2026-01-17 17:35:11 -0500

    Add test for default future behavior

--- test/test_publisher.rb
+++ test/test_publisher.rb
@@ -3,6 +3,24 @@
 require "helper"
 
 class TestPublisher < JekyllUnitTest
+  context "default configuration" do
+    setup do
+      site = fixture_site
+      collection = Jekyll::Collection.new(site, "methods")
+      @thing = Jekyll::Document.new(
+        source_dir("methods/method.md"),
+        :site       => site,
+        :collection => collection
+      )
+      @thing.data["date"] = (Time.now + (60 * 60 * 24)).to_i # tomorrow
+      @publisher = Jekyll::Publisher.new(site)
+    end
+
+    should "be hidden in the future" do
+      assert @publisher.hidden_in_the_future?(@thing)
+    end
+  end
+
   context "when a thing's collection is configured to show future posts" do
     setup do
       site = fixture_site
